144c0e330d993689507a88933d85ebf0337f0339
Yogender Singh <yogendersingh@Yogenders-MacBook-Pro.local>
2021-02-06 13:30:37 +0530

    extra webpack added

---
+++ extra-webpack.config.js
@@ -0,0 +1,9 @@
+// const HtmlWebpackPlugin = require("html-webpack-plugin")
+// const PreloadWebpackPlugin = require('@vue/preload-webpack-plugin');
+
+// module.exports = {
+//     plugins: [
+//         new HtmlWebpackPlugin(),
+//         new PreloadWebpackPlugin({})
+//     ]
+// };

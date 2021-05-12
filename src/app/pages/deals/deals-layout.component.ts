a714b4a496e7fd2f1d3a3c629d8125e39f681169
Yogender-moglix <48681429+Yogender-moglix@users.noreply.github.com>
2021-05-12 16:28:37 +0530

    deals scss changes

--- src/app/pages/deals/deals-layout.component.ts
+++ src/app/pages/deals/deals-layout.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-deals-layout',
   templateUrl: './deals-layout.component.html',
-  styleUrls: ['./deals-layout.component.css']
+  styleUrls: ['./deals-layout.component.scss']
 })
 export class DealsLayoutComponent implements OnInit {
 

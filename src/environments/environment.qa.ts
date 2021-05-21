4ac456aa063ca5f03d8027a07fa4cf24f4dde435
Yogender Singh <yogendersingh@Yogenders-MBP.Dlink>
2021-05-21 16:38:56 +0530

    VPN, QA env configured

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   PROD_NAME: 'https://www.moglix.com', 
-  BASE_URL: 'https://apinew.moglix.com/nodeApi/v1',
-  //BASE_URL: 'https://nodeapiqa.moglilabs.com/nodeApi/v1',
+  //BASE_URL: 'https://apinew.moglix.com/nodeApi/v1',
+  BASE_URL: 'https://nodeapiqa.moglilabs.com/nodeApi/v1',
   IMAGE_ASSET_URL: 'https://cdn.moglix.com/mob/',
   SOCKET_URL: "https://socketqa.moglilabs.com",
   IMAGE_BASE_URL: 'https://cdn.moglix.com/',

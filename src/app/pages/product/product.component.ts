ed3c096c77d9c9358da8d84c7944cb2b976464fc
Anilkumar G <anil.payasia@moglix.com>
2021-05-28 16:21:14 +0530

    fix forurl issue

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1665,9 +1665,9 @@ export class ProductComponent implements OnInit, AfterViewInit {
     }
 
     if (this.productOutOfStock == true) {
-      this.pageTitle.setTitle("Buy " + title + " Online At Best Price On Moglix");
+      this.pageTitle.setTitle("Buy " + pageTitleName + " Online At Best Price On Moglix");
     } else {
-      this.pageTitle.setTitle("Buy " + title + " Online At Price ₹" + this.productPrice);
+      this.pageTitle.setTitle("Buy " + pageTitleName + " Online At Price ₹" + this.productPrice);
     }
 
     let metaDescription = '';
@@ -1749,7 +1749,7 @@ export class ProductComponent implements OnInit, AfterViewInit {
 
   setProductSeoSchema() {
 
-    if (true && this.rawProductData) {
+    if (this.isServer && this.rawProductData) {
       let inStock = (!this.productOutOfStock) ? "http://schema.org/InStock" : "http://schema.org/OutOfStock";
       let reviewCount = this.reviews.summaryData.review_count > 0 ? this.reviews.summaryData.review_count : 1;
       let ratingValue = this.reviews.summaryData.final_average_rating > 0 ? this.reviews.summaryData.final_average_rating : 3.5;
@@ -1795,7 +1795,7 @@ export class ProductComponent implements OnInit, AfterViewInit {
           },
           "offers": {
             "@type": "Offer",
-            "url": CONSTANTS.PROD + '/' + this.productUrl,
+            "url": CONSTANTS.PROD + '/' + this.getProductURL(),
             "priceCurrency": "INR",
             "price": (this.productPrice * this.productMinimmumQuantity).toString(),
             "itemCondition": CONSTANTS.SCHEMA + "/NewCondition",

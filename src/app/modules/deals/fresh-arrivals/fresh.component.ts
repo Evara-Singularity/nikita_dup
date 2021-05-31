1a39caa153895e93eb2035b1555d70ea83c54516
Vaibhav013 <vaibhav.sharma@moglix.com>
2021-05-31 07:53:41 +0530

    deals css

--- src/app/modules/deals/fresh-arrivals/fresh.component.ts
+++ src/app/modules/deals/fresh-arrivals/fresh.component.ts
@@ -1,44 +1,34 @@
-import { Component, ViewEncapsulation, Renderer2, Inject, PLATFORM_ID, ElementRef } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import {Component, ViewEncapsulation, Renderer2, Inject, PLATFORM_ID, ElementRef} from '@angular/core';
+import { Router, ActivatedRoute} from '@angular/router';
 import { isPlatformServer, isPlatformBrowser, DOCUMENT } from '@angular/common';
 import { Title, Meta } from '@angular/platform-browser';
 import CONSTANTS from '@app/config/constants';
 
 @Component({
-  selector: "fresh",
-  templateUrl: "fresh.html",
-  styleUrls: ["fresh.scss"],
-  // encapsulation: ViewEncapsulation.None,
+  selector: 'fresh',
+  templateUrl: 'fresh.html',
+  styleUrls: ['fresh.scss'],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class FreshComponent {
   isServer: boolean;
   isBrowser: boolean;
-  freshData: any;
-
-  constructor(
-    private elementRef: ElementRef,
-    @Inject(PLATFORM_ID) private platformId: Object,
-    @Inject(DOCUMENT) private _document,
-    private _renderer2: Renderer2,
-    private meta: Meta,
-    private activatedRoute: ActivatedRoute,
-    private title: Title,
-    public router: Router,
-    private route: ActivatedRoute) {
-
+  freshData:any;
+  constructor(private elementRef: ElementRef,   @Inject(PLATFORM_ID) private platformId: Object, @Inject(DOCUMENT) private _document, private _renderer2: Renderer2, private meta:Meta,private activatedRoute: ActivatedRoute, private title:Title,public router: Router,private route:ActivatedRoute){
     this.isServer = isPlatformServer(platformId);
-    this.isBrowser = isPlatformBrowser(platformId);
+    this.isBrowser = isPlatformBrowser(platformId); 
     this.getFreshData();
-    this.setMetas();
   }
-  setMetas() {
+
+  ngOnInit(){
     this.title.setTitle("Avail Special Offers and Deals at Moglix.com");
-    this.meta.addTag({ property: "og:title", content: "Avail Special Offers and Deals at Moglix.com", });
-    this.meta.addTag({ property: "og:description", content: "The special offers and, deals that you cannot resist are now on Moglix.com. Enjoy lucrative combos and, discounts, daily/weekly, on premium products." });
-    this.meta.addTag({ property: "og:url", content: CONSTANTS.PROD+"/deals/fresh-arrivals" });
-    this.meta.addTag({ name: "description", content: "The special offers and, deals that you cannot resist are now on Moglix.com. Enjoy lucrative combos and, discounts, daily/weekly, on premium products." });
+    this.meta.addTag({"property":"og:title","content":"Avail Special Offers and Deals at Moglix.com"});
+    this.meta.addTag({"property":"og:description","content":"The special offers and, deals that you cannot resist are now on Moglix.com. Enjoy lucrative combos and, discounts, daily/weekly, on premium products."});
+    this.meta.addTag({"property":"og:url","content":"https://www.moglix.com/deals/fresh-arrivals"});
+    this.meta.addTag({"name":"description","content":"The special offers and, deals that you cannot resist are now on Moglix.com. Enjoy lucrative combos and, discounts, daily/weekly, on premium products."});
     if (this.isServer) {
-      let links = this._renderer2.createElement("link");
+      let links = this._renderer2.createElement('link');
       links.rel = "canonical";
       let href = CONSTANTS.PROD + this.router.url.split("?")[0].split("#")[0].toLowerCase();
       links.href = href;
@@ -81,7 +71,7 @@ export class FreshComponent {
     }
   }
 
-  getFreshData() {
+  getFreshData(){
     // data received by layout resolver
     this.route.data.subscribe(
       (rawData) => {
@@ -102,28 +92,26 @@ export class FreshComponent {
       }
     );
   }
-
   reinsertLinks() {
-    const links = <HTMLAnchorElement[]>(
-      this.elementRef.nativeElement.getElementsByTagName("a")
-    );
-    if (links) {
-      const linksInitialLength = links.length;
-      for (let i = 0; i < linksInitialLength; i++) {
-        const link = links[i];
-
-        if (link.host === window.location.host) {
-          this._renderer2.listen(link, "click", (event) => {
-            event.preventDefault();
-            this.router.navigate([
-              link.href
-                .replace(link.host, "")
-                .replace(link.protocol, "")
-                .replace("//", ""),
-            ]);
-          });
-        }
-      }
-    }
+		const links = <HTMLAnchorElement[]>this.elementRef.nativeElement.getElementsByTagName('a');
+	
+		if (links) {
+		  const linksInitialLength = links.length;
+		  for (let i = 0; i < linksInitialLength; i++) {
+			const link = links[i];
+	
+			if (link.host === window.location.host) {
+			  this._renderer2.listen(link, 'click', event => {
+				event.preventDefault();
+				this.router.navigate([
+				  link.href
+					.replace(link.host, '')
+					.replace(link.protocol, '')
+					.replace('//', '')
+				]);
+			  });
+			}
+		  }
+		}
   }
 }

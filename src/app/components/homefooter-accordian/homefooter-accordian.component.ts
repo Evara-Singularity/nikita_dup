bd23a8ffce4571512b7c1c65750c940098d5e2f2
Anilkumar G <anil.payasia@moglix.com>
2021-06-03 18:15:53 +0530

    removed unwanted code

--- src/app/components/homefooter-accordian/homefooter-accordian.component.ts
+++ src/app/components/homefooter-accordian/homefooter-accordian.component.ts
@@ -1,5 +1,4 @@
-import { CommonService } from '@services/common.service';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import CONSTANTS from '@app/config/constants';
 
 @Component({
@@ -7,20 +6,10 @@ import CONSTANTS from '@app/config/constants';
   templateUrl: './homefooter-accordian.component.html',
   styleUrls: ['./homefooter-accordian.component.scss']
 })
-export class HomefooterAccordianComponent implements OnInit {
+export class HomefooterAccordianComponent {
   @Input() flyoutData;
   @Input() carouselData;
   produrl = CONSTANTS.PROD;
   dataArray = CONSTANTS.SEO_HOME_ACCORDIANS;
-  constructor(public _commonService: CommonService) { }
-
-  ngOnInit(): void {
-    // console.log(this.carouselData);
-    // this.dataArray = this.dataArray.filter(item => {
-    //   this.carouselData[item]['data']['brand_block'].length > 0
-    // });
-    // console.log(this.dataArray);
-  }
-
 }
 

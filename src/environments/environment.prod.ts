fad1cd1b3557fbfd1ba9dc30ab48b50033ccc043
Yogender Singh <yogendersingh@Yogenders-MBP.Dlink>
2021-06-04 20:54:29 +0530

    bundles url updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   PROD_NAME: 'https://www.moglix.com', 
   BASE_URL: 'https://apinew.moglix.com/nodeApi/v1',
   //BASE_URL: 'https://nodeapiqa.moglilabs.com/nodeApi/v1',
-  IMAGE_ASSET_URL: 'https://cdn.moglix.com/online/prod/pwa/',
+  IMAGE_ASSET_URL: 'https://cdn.moglix.com/online/prod/pwa/bundles/',
   SOCKET_URL: "https://socket.moglix.com",
   IMAGE_BASE_URL: 'https://cdn.moglix.com/',
   DOCUMENT_URL: 'https://document.moglix.com/',

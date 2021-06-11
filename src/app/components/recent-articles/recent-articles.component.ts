22ee40b0b48616a218bdd315688835cd40a472df
Pradeep <sai.pradeep@moglix.com>
2021-06-11 15:09:02 +0530

    code changes

--- src/app/components/recent-articles/recent-articles.component.ts
+++ src/app/components/recent-articles/recent-articles.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { Component, Input, NgModule } from "@angular/core";
+import { Component, Input, NgModule, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { CONSTANTS } from '@config/constants';
 
@@ -9,8 +9,12 @@ import { CONSTANTS } from '@config/constants';
     templateUrl: './recent-articles.component.html',
     styleUrls: ['./recent-articles.component.scss'],
 })
-export class RecentArticles {
+export class RecentArticles implements OnInit {
     constructor(private router: Router) {}
+    ngOnInit(): void
+    {
+        console.log(this.recentArticles);
+    }
     @Input() recentArticles;
     @Input() title;
     imageBaseUrl = CONSTANTS.IMAGE_BASE_URL;
@@ -22,7 +26,8 @@ export class RecentArticles {
 
 @NgModule({
     declarations: [RecentArticles],
-    imports: [CommonModule]
+    imports: [CommonModule],
+    exports: [RecentArticles]
 })
 export class RecentArticlesModule {}
 export class CategoryModule extends RecentArticlesModule {}
--- src/app/utils/resolvers/category.resolver.ts
+++ src/app/utils/resolvers/category.resolver.ts
@@ -110,22 +110,26 @@ export class CategoryResolver implements Resolve<object> {
         const FAQ_KEY: any = makeStateKey<{}>('faq-' + categoryId);
         const BREADCRUMP_KEY: any = makeStateKey<{}>('breadcrump-' + categoryId);
         const CMS_KEY: any = makeStateKey<{}>('cms-' + categoryId);
+        const RELATED_ARTICLES_KEY = makeStateKey<{}>('related_articles-' + categoryId);
         
-        if (this.transferState.hasKey(GET_RELATED_CATEGORY_KEY) && this.transferState.hasKey(REFRESH_KEY) && this.transferState.hasKey(FAQ_KEY) && this.transferState.hasKey(BREADCRUMP_KEY) && this.transferState.hasKey(CMS_KEY)) {
+        if (this.transferState.hasKey(GET_RELATED_CATEGORY_KEY) && this.transferState.hasKey(REFRESH_KEY) && this.transferState.hasKey(FAQ_KEY) && this.transferState.hasKey(BREADCRUMP_KEY) && this.transferState.hasKey(CMS_KEY)
+            && this.transferState.hasKey(RELATED_ARTICLES_KEY)) {
             const GET_RELATED_CATEGORY_KEY_OBJ = this.transferState.get<{}>(GET_RELATED_CATEGORY_KEY, null);
             const REFRESH_KEY_OBJ = this.transferState.get<{}>(REFRESH_KEY, null);
             const FAQ_KEY_OBJ = this.transferState.get<{}>(FAQ_KEY, null);
             const BREADCRUMP_KEY_OBJ = this.transferState.get<{}>(BREADCRUMP_KEY, null);
             const CMS_KEY_OBJ = this.transferState.get<{}>(CMS_KEY, null);
+            const RELATED_ARTICLES_OBJ = this.transferState.get<{}>(RELATED_ARTICLES_KEY, {});
             
             this.transferState.remove(GET_RELATED_CATEGORY_KEY);
             this.transferState.remove(REFRESH_KEY);
             this.transferState.remove(FAQ_KEY);
             this.transferState.remove(BREADCRUMP_KEY);
             this.transferState.remove(CMS_KEY);
+            this.transferState.remove(RELATED_ARTICLES_KEY);
             
             this.loaderService.setLoaderState(false);
-            return of([GET_RELATED_CATEGORY_KEY_OBJ, REFRESH_KEY_OBJ, FAQ_KEY_OBJ, BREADCRUMP_KEY_OBJ, CMS_KEY_OBJ]);
+            return of([GET_RELATED_CATEGORY_KEY_OBJ, REFRESH_KEY_OBJ, FAQ_KEY_OBJ, BREADCRUMP_KEY_OBJ, CMS_KEY_OBJ, RELATED_ARTICLES_OBJ]);
         } else {
             const currentQueryParams = _activatedRouteSnapshot.queryParams;
             const params = _activatedRouteSnapshot.params;
@@ -135,23 +139,23 @@ export class CategoryResolver implements Resolve<object> {
             const faq_url = CONSTANTS.NEW_MOGLIX_API + ENDPOINTS.GET_CATEGORY_SCHEMA + "?categoryCode=" + categoryId;
             const breadcrump_url = CONSTANTS.NEW_MOGLIX_API + ENDPOINTS.BREADCRUMB + "?source=" + source + "&type=category";
             const cms_url = CONSTANTS.NEW_MOGLIX_API + ENDPOINTS.GET_CMS_CONTROLLED + "?requestParam=article-1&categoryCode=" + categoryId;
+            
 
             const getRelatedCategoriesObs = this.http.get(get_rel_cat_url).pipe(share());
             const getFAQObs = this.http.get(faq_url).pipe(share());
             const refreshProductsObs = this.refreshProducts(currentQueryParams, params, fragment).pipe(share());
             const getBreadCrump = this.http.get(breadcrump_url).pipe(share());
             const getCmsDynamicDataForCategoryAndBrandObs = this.http.get(cms_url).pipe(share());
-
-
             const apiList = [getRelatedCategoriesObs, refreshProductsObs, getFAQObs, getBreadCrump, getCmsDynamicDataForCategoryAndBrandObs];
-
+            if (!(currentQueryParams['page']) || currentQueryParams['page'] == 1){
+                this.appendPageOneAPIs(apiList, { categoryId: categoryId});
+            }
             if (this._router.url.search('#') < 0) {
                 apiList.push(getCmsDynamicDataForCategoryAndBrandObs)
             } else {
                 this._commonService.cmsData = null;
                 this._commonService.replaceHeading = false;
             }
-
             return forkJoin(apiList).pipe(
                 catchError((err) => {
                     this.loaderService.setLoaderState(false);
@@ -164,11 +168,18 @@ export class CategoryResolver implements Resolve<object> {
                         this.transferState.set(FAQ_KEY, result[2]);
                         this.transferState.set(BREADCRUMP_KEY, result[3]);
                         this.transferState.set(CMS_KEY, result[4] || []);
+                        this.transferState.set(RELATED_ARTICLES_KEY, result[5] || {});
                         this.loaderService.setLoaderState(false);
                     }
                 })
             );
         }
     }
-    
+
+    appendPageOneAPIs(apiList:any[], data){
+        const get_rel_articles_url = CONSTANTS.NEW_MOGLIX_API + ENDPOINTS.GET_RELATED_ARTICLES + `${data.categoryId}`;
+        const getRelatedArticles = this.http.get(get_rel_articles_url).pipe(share());
+        apiList.push(getRelatedArticles);
+        return apiList;
+    }
 }

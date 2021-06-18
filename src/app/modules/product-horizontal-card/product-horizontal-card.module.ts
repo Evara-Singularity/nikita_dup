ba195d1c037ed6237443a4e9a79eb044d1c4c7e5
Yogender Singh <yogendersingh@Yogenders-MBP.Dlink>
2021-06-18 15:15:24 +0530

    loader fixed shared PLP

--- src/app/modules/product-horizontal-card/product-horizontal-card.module.ts
+++ src/app/modules/product-horizontal-card/product-horizontal-card.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductHorizontalCardComponent } from './product-horizontal-card.component';
+import { RatingPipeModule } from '@app/utils/pipes/rating.pipe';
 
 @NgModule({
   declarations: [ProductHorizontalCardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RatingPipeModule
   ],
   exports: [
     ProductHorizontalCardComponent

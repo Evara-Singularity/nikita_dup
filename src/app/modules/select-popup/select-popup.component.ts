d5379d6e6e4fa9a200d15e3f9de5db3f8c4d795b
Yogender Singh <yogendersingh@Yogenders-MBP.Dlink>
2021-06-28 18:51:29 +0530

    credit and debit card completed

---
+++ src/app/modules/select-popup/select-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-popup',
+  templateUrl: './select-popup.component.html',
+  styleUrls: ['./select-popup.component.css']
+})
+export class SelectPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/select-popup/select-popup.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SelectPopupComponent } from './select-popup.component';
+
+
+
+@NgModule({
+  declarations: [SelectPopupComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SelectPopupModule { }

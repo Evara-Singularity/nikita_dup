e87839438667ee361f93a97abf0b40addb6b6b36
abhi-moglix <abhishek.kumar11@moglix.com>
2021-06-30 13:24:44 +0530

    added search top sections

--- src/app/pages/search-v1/search-v1.component.ts
+++ src/app/pages/search-v1/search-v1.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommonService } from '@app/utils/services/common.service';
 import { ProductListService } from '@services/productList.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'search-v1',
@@ -9,10 +10,14 @@ import { ProductListService } from '@services/productList.service';
   styleUrls: ['./search-v1.component.scss']
 })
 export class SearchV1Component implements OnInit {
+  public API_RESULT: any;
+  public didYouMean: any;
+
   constructor(
     private _activatedRoute: ActivatedRoute,
     public _productListService: ProductListService,
-    private _commonService: CommonService
+    private _commonService: CommonService,
+    private _router: Router
   ) { }
 
   ngOnInit(): void {
@@ -21,10 +26,36 @@ export class SearchV1Component implements OnInit {
 
   setDataFromResolver() {
     this._activatedRoute.data.subscribe(result => {
-      this._productListService.createAndProvideDataToSharedListingComponent(result['searchData'][0], 'Search Results');
+      this.API_RESULT = result;
 
-      this._commonService.selectedFilterData.totalCount = result['search'][0].productSearchResult.totalCount;
+      this._productListService.createAndProvideDataToSharedListingComponent(this.API_RESULT['searchData'][0], 'Search Results');
+
+      this._commonService.selectedFilterData.totalCount = this.API_RESULT['searchData'][0].productSearchResult.totalCount;
+
+      if (this._activatedRoute.snapshot.queryParams["didYouMean"] != undefined) {
+        this.didYouMean = this._activatedRoute.snapshot.queryParams["didYouMean"];
+      }
     });
+  }
+
+  toggleRcommendFlag = true;
+  recommendedCategory: string = '';
+  goToRecommendedCategory(categoryId, cat) {
+    this.toggleRcommendFlag = false;
+    this.recommendedCategory = cat['categoryName'];
+    let extras = {
+      queryParams: { ...this._activatedRoute.snapshot.queryParams }
+    };
+    extras['queryParams']['search_query'] = this.API_RESULT['searchData'][0].productSearchResult.correctedSearchString ? this.API_RESULT['searchData'][0].productSearchResult.correctedSearchString : extras['queryParams']['search_query'];
+    extras['queryParams']['category'] = categoryId;
+    extras['queryParams']['toggleRcommendFlag'] = true;
+    delete extras['queryParams']['orderWay'];
+    delete extras['queryParams']['orderBy'];
+    this._router.navigate(['search'], extras);
+  }
+
+  redirectWithNoPreProcessRequiredParam(searchTerm: string) {
+    console.log(searchTerm);
   }
 
 }

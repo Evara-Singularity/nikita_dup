64eae89c1d986a31f34b2c8ba8949432c20231f4
abhi-moglix <abhishek.kumar11@moglix.com>
2021-07-07 19:31:06 +0530

    brand decor URI fix

--- src/app/utils/resolvers/brand-v1.resolver.ts
+++ src/app/utils/resolvers/brand-v1.resolver.ts
@@ -71,7 +71,8 @@ export class BrandV1Resolver implements Resolve<any> {
       };
       
       const actualParams = this._commonService.formatParams(params);
-      actualParams['brand'] = _activatedRouteSnapshot.params.brand;
+      actualParams['brand'] = _activatedRouteSnapshot.params.brand.split('-').join(' & ');
+      console.log(actualParams['brand']);
 
       if (_activatedRouteSnapshot.params.hasOwnProperty('category')) {
         CMS_DATA_API_URL += '&categoryCode=' + _activatedRouteSnapshot.params.category;

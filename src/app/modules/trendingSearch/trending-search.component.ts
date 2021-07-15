32815493175a2eeb28587ade1f2874abbde59ace
Abhishek Kumar <79142369+abhi-moglix@users.noreply.github.com>
2021-07-15 20:39:42 +0530

    updated

--- src/app/modules/trendingSearch/trending-search.component.ts
+++ src/app/modules/trendingSearch/trending-search.component.ts
@@ -22,8 +22,8 @@ export class TrendingSearchComponent {
     ngOnInit() {
         this._service.getTrendingCategories().subscribe((data: any) => {
             this.trendingCat = data.data[0]['block_data'];
-            this.trendingCatName = this.getTrendingCategories(this.trendingCat['top_categories']);
-            this.topCategories = this.getTopCategories(this.trendingCat['trending_search']);
+            this.trendingCatName = this.getTrendingCategories(this.trendingCat['trending_search']);
+            this.topCategories = this.getTopCategories(this.trendingCat['top_categories']);
         });
     }
 

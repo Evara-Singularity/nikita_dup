8f0e4d90a0c828364150340113cc084c03fe8e35
Yogender Singh <yogendersingh@Yogenders-MBP.Dlink>
2021-07-30 21:46:21 +0530

    intial css and script bundles are preloaded in index.html

--- index-html-transform.js
+++ index-html-transform.js
@@ -1,5 +1,5 @@
 const cssLinkRegExp = /<link rel="stylesheet" href="(.+)">/;
-const scriptLinkRegExp = /<script.*?src="(.*?)(main-|polyfills-|runtime-)(.*?)"/g
+const scriptLinkRegExp = /<script.*?src="(.*?)(main-|polyfills-|runtime-|vendor-)(.*?)"/g
 
 function getAsyncLink(href) {
     return `
@@ -13,24 +13,28 @@ function getHref(indexHtml) {
 }
 
 
-module.exports = (_, indexHtml) => {
+module.exports = (targetOptions, indexHtml) => {
     const headClosingTagIdx = indexHtml.indexOf('</head>');
-
     // add rel=preload tag for bundle CSS
     const headPart = indexHtml.slice(0, headClosingTagIdx);
     const asyncLinkPart = getAsyncLink(getHref(indexHtml));
     const bodyPart = indexHtml.slice(headClosingTagIdx);
     // add rel=preload tags for intial bundle CSS
-    const scriptPreloadTags = indexHtml.match(scriptLinkRegExp).map((val) => {
+    let scriptPreloadTags = [];
+    scriptPreloadTags = indexHtml.match(scriptLinkRegExp).map((val) => {
         return val.replace('<script src="', '').replace('"', '');
     }).map(scriptURL => {
-        return `<link rel="script" href="${scriptURL}"/>`
+        return `<link rel="preload" as="script" href="${scriptURL}"/>
+        `;
     });
+    // if (targetOptions.configuration.includes('production') || targetOptions.configuration.includes('qa')) {
+    // }
+
 
     return `
             ${headPart.replace(cssLinkRegExp, '')}
             ${asyncLinkPart}
-            ${scriptPreloadTags}
+            ${scriptPreloadTags.join('')}
             ${bodyPart}
-            `;
+        `;
 };

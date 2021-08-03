a44116cddc8b5b7136fb93f1293a40ad2a5feccb
Yogender Singh <yogendersingh@Yogenders-MacBook-Pro.local>
2021-08-03 18:34:14 +0530

    initialNavigation routing fixing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
+  imports: [RouterModule.forRoot(routes, { initialNavigation: 'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

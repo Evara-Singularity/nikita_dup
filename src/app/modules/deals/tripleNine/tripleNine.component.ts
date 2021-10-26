d593bd54765dc4792be0870bbae5c1c55ea9ab6e
abhi-moglix <abhishek.kumar11@moglix.com>
2021-10-26 12:01:27 +0530

    resolved tab bug

--- src/app/modules/deals/tripleNine/tripleNine.component.ts
+++ src/app/modules/deals/tripleNine/tripleNine.component.ts
@@ -21,7 +21,6 @@ export class TripleNineComponent {
     private _renderer2: Renderer2,
     private title: Title,
     public router: Router,
-    @Inject(DOCUMENT) private _document,
     private meta: Meta,
     private route: ActivatedRoute,
     public _commonService:CommonService,

276f729f1ed506ba6dd800a6ffc7ff31169a8df9
Vaibhav013 <vaibhav.sharma@moglix.com>
2021-10-28 15:30:49 +0530

    changes

--- src/app/pages/e-gift-voucher/e-gift-voucher.module.ts
+++ src/app/pages/e-gift-voucher/e-gift-voucher.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from "@angular/common";
 import { RouterModule } from '@angular/router';
 import {EGiftVoucherComponent} from "./e-gift-voucher.component";
 import { EGiftVoucherRoutingModule } from './e-gift-voucher.routing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
         CommonModule,
         EGiftVoucherRoutingModule,
-        RouterModule
+        RouterModule,
+        FormsModule,
+        ReactiveFormsModule
     ],
     declarations: [
       EGiftVoucherComponent

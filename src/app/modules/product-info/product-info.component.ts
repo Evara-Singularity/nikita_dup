3d3ba071be1aa2b9cd0e025e3ce55ef13aeb74da
yajya <yajya.kalra@moglix.com>
2021-11-08 11:41:08 +0530

    specification popup

--- src/app/modules/product-info/product-info.component.ts
+++ src/app/modules/product-info/product-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnDestroy, OnInit, Output } from '@angular/core';
 
 @Component({
     selector: 'product-info',
@@ -14,12 +14,38 @@ export class ProductInfoComponent implements OnInit, OnDestroy
 
     @Output() closePopup$: EventEmitter<any> = new EventEmitter<any>();
     defaultInfo = "";
+    selectedIndex = 0;
+    leftTabIdx = 0;
+    atStart = true;
+    atEnd = false;
+    shiftLeft:string;
 
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() { 
+        this.shiftLeft = `translateX(0px)`;
+    }
 
-    updateTab(tab){this.defaultInfo = tab}
+    updateTab(tab,index){
+        this.selectedIndex = index;
+        this.defaultInfo = tab;
+        console.log(tab, this.selectedIndex);
+        this.scrollTab(index - this.leftTabIdx - 1);
+
+    }
+    scrollTab(x){
+        if ((this.atStart && x < 0) || (this.atEnd && x > 0)) {
+            return;
+          }
+        this.leftTabIdx = this.leftTabIdx + x;
+        this.shiftLeft = `translateX(${this.leftTabIdx * -140}px)`;
+        this.atStart = this.leftTabIdx === 0;
+        this.atEnd = this.leftTabIdx === this.TABS.length - 1
+    }
+    @HostListener('scroll', ['$event']) 
+    scrollHandler(event) {
+      console.debug("Scroll Event");
+    }
 
     close(event) { this.closePopup$.emit(); }
 

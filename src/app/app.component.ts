bee5c251db1955e3b37b1be6931a7949ded5e497
VaibhavShocker <vs.vaibhavs.1995@gmail.com>
2021-11-23 16:09:44 +0530

    browser condition added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonService } from './utils/services/common.service';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,10 @@ export class AppComponent {
 
   pageRefreshed = true;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private _commonService: CommonService) {}
 
   ngAfterViewInit() {
-    if (this.pageRefreshed && window) {
+    if (this.pageRefreshed && this._commonService.isBrowser && window) {
       window.history.replaceState('', '', '/');
       window.history.pushState('', '', this.router.url);
       this.pageRefreshed = false;

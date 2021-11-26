96fda35f37c75037c170a5448dba3b0ac9033e8c
Yogender Singh <yogender.singh@moglix.com>
2021-11-26 17:41:34 +0530

    JIRA 1663 completed | bug fi

--- src/app/modules/product-listing-app-promo/product-listing-app-promo.component.ts
+++ src/app/modules/product-listing-app-promo/product-listing-app-promo.component.ts
@@ -1,11 +1,11 @@
-import { Component, ComponentFactoryResolver, Injector, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, ComponentFactoryResolver, Injector, OnDestroy, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'product-listing-app-promo',
   templateUrl: './product-listing-app-promo.component.html',
   styleUrls: ['./product-listing-app-promo.component.scss']
 })
-export class ProductListingAppPromoComponent implements OnInit {
+export class ProductListingAppPromoComponent implements OnInit, OnDestroy {
 
   // ondemand loaded components for app Promo
   appPromoInstance = null;
@@ -17,20 +17,30 @@ export class ProductListingAppPromoComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+
   }
 
   async onVisibleAppPromo(event) {
-    const { ProductAppPromoComponent } = await import('../../components/product-app-promo/product-app-promo.component')
-    const factory = this.cfr.resolveComponentFactory(ProductAppPromoComponent);
-    this.appPromoInstance = this.appPromoContainerRef.createComponent(factory, null, this.injector);
-    this.appPromoInstance.instance['page'] = 'order-confirmation';
-    this.appPromoInstance.instance['isOverlayMode'] = false;
-    this.appPromoInstance.instance['showPromoCode'] = false;
-    this.appPromoInstance.instance['isLazyLoaded'] = true;
+    if(!this.appPromoInstance){
+      const { ProductAppPromoComponent } = await import('../../components/product-app-promo/product-app-promo.component')
+      const factory = this.cfr.resolveComponentFactory(ProductAppPromoComponent);
+      this.appPromoInstance = this.appPromoContainerRef.createComponent(factory, null, this.injector);
+      this.appPromoInstance.instance['page'] = 'order-confirmation';
+      this.appPromoInstance.instance['isOverlayMode'] = false;
+      this.appPromoInstance.instance['showPromoCode'] = false;
+      this.appPromoInstance.instance['isLazyLoaded'] = true;
+    }
   }
 
   appPromoStatus(event) {
 
+  }
+
+  ngOnDestroy() {
+    if (this.appPromoInstance) {
+      this.appPromoInstance.destroy();
+      this.appPromoContainerRef.remove()    
+    }
   }
 
 }

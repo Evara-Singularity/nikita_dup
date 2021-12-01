7817aee0baf60337e0a41148a2b2b87186121ef0
Vaibhav013 <vaibhav.sharma@moglix.com>
2021-12-01 10:12:21 +0530

    search string encoding fixed

--- src/app/utils/services/product.service.ts
+++ src/app/utils/services/product.service.ts
@@ -42,7 +42,7 @@ export class ProductService {
     }
 
     getSimilarProducts(productName, categoryId) {
-        const URL = this.basePath + ENDPOINTS.SIMILAR_PRODUCTS +'?str=' + encodeURIComponent(productName) + '&category=' + encodeURIComponent(categoryId);
+        const URL = this.basePath + ENDPOINTS.SIMILAR_PRODUCTS +'?str=' + encodeURIComponent(productName) + '&category=' + categoryId;
         return this._dataService.callRestful('GET', URL)
             .pipe(
                 catchError((res: HttpErrorResponse) => {

18e145afe16555bb58ebb08a927af25ed78b2337
yajya <yajya.kalra@moglix.com>
2021-12-08 15:27:50 +0530

    bottom menu common in idle-search-container

--- src/app/modules/Idle-user-search-nudge/Idle-user-search-nudge.module.ts
+++ src/app/modules/Idle-user-search-nudge/Idle-user-search-nudge.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IdleUserSearchNudgeComponent } from './Idle-user-search-nudge.component';
-
+import { BottomMenuModule } from '../bottomMenu/bottom-menu.module';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BottomMenuModule
   ],
   declarations: [IdleUserSearchNudgeComponent],
   exports: [IdleUserSearchNudgeComponent]

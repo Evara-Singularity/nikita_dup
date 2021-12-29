d066719d34535d8cc0a30d1980ca71f65302515a
Pradeep <sai.pradeep@moglix.com>
2021-12-29 17:14:12 +0530

    Added static bread crumb

---
+++ src/app/components/static-breadcrumb/static-breadcrumb.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+    selector: 'static-breadcrumb',
+    templateUrl: './static-breadcrumb.component.html',
+    styleUrls: ['./static-breadcrumb.component.scss'],
+})
+export class StaticBreadcrumbComponent implements OnInit
+{
+
+    @Input('breadCrumbList') breadCrumbList = [];
+    constructor() { }
+    ngOnInit() { }
+}
+
+@NgModule({
+    declarations: [StaticBreadcrumbComponent],
+    imports: [CommonModule, RouterModule],
+    exports: [StaticBreadcrumbComponent]
+})
+export class StaticBreadcrumbModule { }

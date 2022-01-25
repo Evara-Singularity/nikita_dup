81b1f9f7621709944bef69ab49a431f9fdc829f9
Yogender Singh <yogender.singh@moglix.com>
2022-01-25 13:09:23 +0530

    PDP back redirection fix 4

--- src/app/modules/header-nav/header-nav.component.ts
+++ src/app/modules/header-nav/header-nav.component.ts
@@ -366,27 +366,12 @@ export class HeaderNavComponent implements OnInit, OnDestroy, AfterViewInit {
         this.backRedirectUrl = localStorage.getItem('backRedirectUrl');
         const isCheckout = this.backRedirectUrl && this.backRedirectUrl.toLowerCase().includes('checkout');
         if (this.backRedirectUrl && this.backRedirectUrl !== '/' && isCheckout === false) {
-            console.log('back to home 1', window.history.length);  
-            if(window.history.length > 3){
-                console.log('back to home 1.1', this._commonService.currentlyOpenedModule,  this._commonService.currentlyOpenedModuleUsed);
-                if( this._commonService.currentlyOpenedModuleUsed == true){
-                    this.router.navigateByUrl('/?back=1');
-                }else{
-                    this.location.back()
-                }
-            }else{
-                console.log('back to home 1.2', this._commonService.currentlyOpenedModule,  this._commonService.currentlyOpenedModuleUsed);
-                if (this._commonService.currentlyOpenedModule && this._commonService.currentlyOpenedModule.data && this._commonService.currentlyOpenedModule.data.overrideRedirectUrl) {
-                    this._commonService.currentlyOpenedModuleUsed = true;
-                    this.router.navigate([this._commonService.currentlyOpenedModule.data.overrideRedirectUrl], { queryParams: { back: 1 } });
-                } else {
-                    console.log('back to home 1.2.2', this._commonService.currentlyOpenedModule,  this._commonService.currentlyOpenedModuleUsed);
-                    this.location.back();
-                }
-            }
+           // console.log('back to home 1', window.history.length);  
+            this.pdpReirectHack();
         } else {
-            console.log('back to home 2', window.history.length);  
+           // console.log('back to home 2', window.history.length);  
             if (this.staticPages.indexOf(window.location.pathname) !== -1) {
+               // console.log('back to home 2.1', window.history.length);  
                 this.router.navigate(['/']);
             } else if (isCheckout) {
                 if (this.checkoutLoginService.isAtFirstSection) {
@@ -404,7 +389,28 @@ export class HeaderNavComponent implements OnInit, OnDestroy, AfterViewInit {
                     this.checkoutLoginService.enableResetTabSateSub(true);
                 }
             } else {
-                this.router.navigate(['/']);
+               this.pdpReirectHack();
+            }
+        }
+    }
+
+    private pdpReirectHack() {
+        if (window.history.length > 3) {
+           // console.log('back to home 1.1', this._commonService.currentlyOpenedModule, this._commonService.currentlyOpenedModuleUsed);
+            if (this._commonService.currentlyOpenedModuleUsed == true) {
+                this._commonService.currentlyOpenedModuleUsed = false;
+                this.router.navigateByUrl('/?back=1');
+            } else {
+                this.location.back();
+            }
+        } else {
+           // console.log('back to home 1.2', this._commonService.currentlyOpenedModule, this._commonService.currentlyOpenedModuleUsed);
+            if (this._commonService.currentlyOpenedModule && this._commonService.currentlyOpenedModule.data && this._commonService.currentlyOpenedModule.data.overrideRedirectUrl) {
+                this._commonService.currentlyOpenedModuleUsed = true;
+                this.router.navigate([this._commonService.currentlyOpenedModule.data.overrideRedirectUrl], { queryParams: { back: 1 } });
+            } else {
+               // console.log('back to home 1.2.2', this._commonService.currentlyOpenedModule, this._commonService.currentlyOpenedModuleUsed);
+                this.location.back();
             }
         }
     }

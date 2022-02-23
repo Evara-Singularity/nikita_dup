1c63681dcddc3480a730e45ebca67a9521c988a4
Yogender Singh <yogendersingh@Yogenders-MacBook-Pro.local>
2022-02-23 22:14:37 +0530

    dummy fix for comp time error

--- src/app/components/shared-cart-v2/cart/cart.component.ts
+++ src/app/components/shared-cart-v2/cart/cart.component.ts
@@ -65,6 +65,11 @@ export class CartComponent {
             console.log(this.cartData);
         }
 
+        // added dummy function to resolve compile time error
+        redirectToProductURL(showLink, productUrl){
+
+        }
+
         updateCartItemQuantity(quantityTarget, index, action, buyNow = false) {
             if (quantityTarget < 1) return;
             if (action = 'increment') {

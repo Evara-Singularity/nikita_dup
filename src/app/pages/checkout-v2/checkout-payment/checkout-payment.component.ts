4809cc22e6c82b58efb5de7947aaac4f870b02d5
Pradeep <sai.pradeep@moglix.com>
2022-03-11 11:36:21 +0530

    Fixed css file issue

--- src/app/pages/checkout-v2/checkout-payment/checkout-payment.component.ts
+++ src/app/pages/checkout-v2/checkout-payment/checkout-payment.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-checkout-payment',
   templateUrl: './checkout-payment.component.html',
-  styleUrls: ['./checkout-payment.component.css']
+  styleUrls: ['./checkout-payment.component.scss']
 })
 export class CheckoutPaymentComponent implements OnInit {
 

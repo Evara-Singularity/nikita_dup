bc0567ac1cfbc04c3de585c913c8e77bc7a45330
Yogender Singh <yogendersingh@Yogenders-MacBook-Pro.local>
2022-03-17 12:05:17 +0530

    minor CSS changes

--- src/app/pages/checkout-v2/checkout-payment/checkout-payment.component.ts
+++ src/app/pages/checkout-v2/checkout-payment/checkout-payment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from '@app/utils/services/cart.service';
 
 @Component({
   selector: 'app-checkout-payment',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutPaymentComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _cartService: CartService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.checkForValidRedirection();
+  }
+
+  checkForValidRedirection() {
+    if (
+      (this._cartService.invoiceType == 'retail' && this._cartService.shippingAddress) ||
+      (this._cartService.invoiceType == 'tax' && this._cartService.shippingAddress && this._cartService.billingAddress)
+    ) {
+      console.log('payment logs', this._cartService.invoiceType, this._cartService.shippingAddress, this._cartService.billingAddress)
+    } else {
+      this.router.navigate(['/checkout/address']);
+    }
   }
 
 }

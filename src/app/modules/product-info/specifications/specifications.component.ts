57d353378f519c655bc13f408b631e1404075533
yajya <yajya.kalra@moglix.com>
2022-03-29 13:01:44 +0530

    show more html

--- src/app/modules/product-info/specifications/specifications.component.ts
+++ src/app/modules/product-info/specifications/specifications.component.ts
@@ -1,18 +1,32 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonService } from '@app/utils/services/common.service';
-
+import { fade } from '@utils/animations/animation';
 @Component({
     selector: 'specifications',
     templateUrl: './specifications.component.html',
-    styleUrls: ['./specifications.component.scss']
+    styleUrls: ['./specifications.component.scss'],
+    animations: [
+        fade
+    ]
 })
 export class SpecificationsComponent implements OnInit
 {
     @Input("specifications") specifications = null;
-
+    public isAllListShow:boolean;
+    showtText:string="SHOW MORE";
+    
     constructor(public _commonService: CommonService) { }
 
     ngOnInit() {
     }
-
+    showMore() {
+        this.isAllListShow = !this.isAllListShow;
+        if(this.isAllListShow)
+        {
+            this.showtText="SHOW LESS";
+        }
+        else{
+            this.showtText="SHOW MORE";
+        }
+    }
 }

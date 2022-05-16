986255d9b8f1083cc542483493c0e97e4de748cf
Moglix <nikita.yadav@moglix.com>
2022-05-16 15:56:45 +0530

    footer accordiaon

--- src/app/components/homefooter-accordian/homefooter-accordian.component.ts
+++ src/app/components/homefooter-accordian/homefooter-accordian.component.ts
@@ -5,6 +5,10 @@ import { CONSTANTS } from '@app/config/constants';
 import { SiemaCarouselModule } from '@app/modules/siemaCarousel/siemaCarousel.module';
 import { KpToggleDirectiveModule } from '@app/utils/directives/kp-toggle.directive';
 import { CommonService } from '@app/utils/services/common.service';
+import { AccordiansDetails,AccordianDataItem } from '@app/utils/models/accordianInterface';
+import { AccordianModule } from "@app/modules/accordian/accordian.module";
+
+
 
 @Component({
   selector: 'home-accordians',
@@ -16,6 +20,29 @@ export class HomefooterAccordianComponent {
   @Input() carouselData;
   produrl = CONSTANTS.PROD;
   dataArray = CONSTANTS.SEO_HOME_ACCORDIANS;
+  accordiansDetails:AccordiansDetails[]=[];
+
+
+  ngOnInit() {
+    this.footerAccordian();
+  }
+
+  footerAccordian() {
+    this.accordiansDetails = [];
+    this.dataArray.forEach(data => {
+      Object.entries(this.carouselData).forEach(element => {
+        if (element[0] == data) {
+          let blockData = element[1]["data"]["brand_block"]
+          this.accordiansDetails.push({
+            name: "Brands From " + element[1]["layout_name"],
+            data: blockData.map(x => ({ name: x.brandName, link: x.brand_url }) as AccordianDataItem),
+            icon: ''
+          });
+        }
+      });
+    });
+  }
+
 }
 
 
@@ -24,7 +51,8 @@ export class HomefooterAccordianComponent {
     CommonModule,
     RouterModule,
     SiemaCarouselModule,
-    KpToggleDirectiveModule
+    KpToggleDirectiveModule,
+    AccordianModule
   ],
   declarations: [
     HomefooterAccordianComponent

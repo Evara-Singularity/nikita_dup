ac54c596fcb87dd33bf69785ed363f259f23f7af
Moglix <nikita.yadav@moglix.com>
2022-05-25 17:32:35 +0530

    modification

--- src/app/utils/resolvers/about.resolver.ts
+++ src/app/utils/resolvers/about.resolver.ts
@@ -11,12 +11,6 @@ import { makeStateKey, TransferState } from '@angular/platform-browser';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { isPlatformServer } from '@angular/common';
 
-
-
-
-
-
-
 @Injectable({
   providedIn: 'root'
 })
@@ -24,40 +18,31 @@ import { isPlatformServer } from '@angular/common';
 
 export class AboutResolver implements Resolve<boolean> {
 
-
   constructor(
-    
     @Inject(PLATFORM_ID) private platformId,
     private http: HttpClient,
     private transferState: TransferState,
-
-
-)
-{ }
-
+  ) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
 
-    const ABOUT_US_KEY = makeStateKey<any>('AUDST');
+    const ABOUT_US_KEY = makeStateKey<any>('ABOUTUS');
     if (this.transferState.hasKey(ABOUT_US_KEY)) {
-        const response = this.transferState.get<any>(ABOUT_US_KEY, null);
-        this.transferState.remove(ABOUT_US_KEY);
-        return of(response);
+      const response = this.transferState.get<any>(ABOUT_US_KEY, null);
+      this.transferState.remove(ABOUT_US_KEY);
+      return of(response);
     }
     else {
-          const url = CONSTANTS.NEW_MOGLIX_API + '/category/getparentcategoryjsonbody?requestType=about-us_d';
-        return this.http.get(url)
-            .pipe(
-                catchError((err) => { return of(err); }),
-                tap(response =>
-                {
-                    if (isPlatformServer(this.platformId)) {
-                        this.transferState.set(ABOUT_US_KEY, response);
-                    }
-                })
-            );
+      const url = CONSTANTS.NEW_MOGLIX_API + '/category/getparentcategoryjsonbody?requestType=about-us_d';
+      return this.http.get(url)
+        .pipe(
+          catchError((err) => { return of(err); }),
+          tap(response => {
+            if (isPlatformServer(this.platformId)) {
+              this.transferState.set(ABOUT_US_KEY, response);
+            }
+          })
+        );
     }
   }
-
-
 }

fcc757d6379e927efb723835ca2d9f146a6605f8
Pradeep <sai.pradeep@moglix.com>
2022-06-13 16:26:38 +0530

    Promocode design changes

--- src/app/modules/shared-checkout-quick-order-components/promoCode/promoCode.module.ts
+++ src/app/modules/shared-checkout-quick-order-components/promoCode/promoCode.module.ts
@@ -3,12 +3,14 @@ import { CustomPromoCodeComponent } from './custom-promo-code/custom-promo-code.
 import { PromoCodeListComponent } from './promo-code-list/promo-code-list.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { PopUpModule } from '@app/modules/popUp/pop-up.module';
 
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
+        PopUpModule
     ],
     exports: [
         CustomPromoCodeComponent,

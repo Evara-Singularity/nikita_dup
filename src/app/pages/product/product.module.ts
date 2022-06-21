c96575eede406f0c1a0004f58a41203a1fd62678
sachin07101994 <sachin.chauhan@moglix.com>
2022-06-21 18:22:32 +0530

    new rfq popup sone

--- src/app/pages/product/product.module.ts
+++ src/app/pages/product/product.module.ts
@@ -32,6 +32,7 @@ import { ProductReviewModule } from '@app/components/product-review/product-revi
 import { ProductQaModule } from '@app/components/product-qa/product-qa.component';
 import { FloatingButtonContainerModule } from '@app/modules/ui/floating-button-container/floating-button-container.module';
 import { ProductBenefitsModule } from '@app/components/product-benefits/product-benefits.component';
+import { BottomMenuModule } from '@app/modules/bottomMenu/bottom-menu.module';
 import { ProductGetQuoteModule } from '@app/components/product-get-quote/product-get-quote.component';
 import { ProductGroupingAttributesComponent, ProductGroupingAttributesModule } from '@app/components/product-grouping-attributes/product-grouping-attributes.component';
 @NgModule({
@@ -39,6 +40,7 @@ import { ProductGroupingAttributesComponent, ProductGroupingAttributesModule } f
   imports: [
     ObserveVisibilityDirectiveModule,
     CommonModule,
+    BottomMenuModule,
     ProductRoutingModule,
     ProductOosSimilarModule,
     BreadcrumbNavModule,

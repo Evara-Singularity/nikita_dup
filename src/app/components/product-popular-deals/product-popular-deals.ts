3b70f7a1b7f110e3ca391b70ed0a264dbea6fdba
Vaibhav <vaibhav.sharma@moglix.com>
2022-06-22 12:15:25 +0530

    sprint-20-ui-revamp

---
+++ src/app/components/product-popular-deals/product-popular-deals.ts
@@ -0,0 +1,92 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, NgModule, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from '../../utils/services/product.service';
+import { MathCeilPipeModule } from '../../utils/pipes/math-ceil';
+import { MathFloorPipeModule } from '../../utils/pipes/math-floor';
+import { CommonService } from '../../utils/services/common.service';
+import CONSTANTS from '@app/config/constants';
+import { ClientUtility } from '@app/utils/client.utility';
+import { ProductCardFeature, ProductCardMetaInfo, ProductsEntity } from '@app/utils/models/product.listing.search';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
+import { ProductCardVerticalGridViewModule } from '@app/modules/product-card/product-card-vertical-grid-view/product-card-vertical-grid-view.module';
+import { ProductCardVerticalContainerModule } from '@app/modules/ui/product-card-vertical-container/product-card-vertical-container.module';
+
+@Component({
+    selector: 'product-popular-deals',
+    templateUrl: './product-popular-deals.html',
+    styleUrls: ['./product-popular-deals.scss']
+})
+export class ProductPopularDealsComponent implements OnInit {
+    readonly imagePath = CONSTANTS.IMAGE_BASE_URL;
+    polpularDealsProducts: ProductsEntity[] = null;
+    @Input('outOfStock') outOfStock = false;
+    @Input('partNumber') partNumber;
+    @Input('groupId') groupId;
+    @Input('productName') productName;
+    @Input('categoryCode') categoryCode;
+    @Input('analytics') analytics = null;
+    @Output('popularDealsDataLoaded$') popularDealsDataLoaded$ = new EventEmitter();
+    readonly cardFeaturesConfig: ProductCardFeature = {
+        // feature config
+        enableAddToCart: true,
+        enableBuyNow: true,
+        enableFeatures: false,
+        enableRating: true,
+        enableVideo: false,
+        // design config
+        enableCard: true,
+        verticalOrientation: true,
+        horizontalOrientation: false,
+        lazyLoadImage: false
+    }
+    cardMetaInfo: ProductCardMetaInfo = null;
+
+    constructor(
+        public commonService: CommonService,
+        private router: Router,
+        private productService: ProductService,
+    ) { }
+
+    ngOnInit(): void {
+        this.getProductPopularDeals();
+        this.cardMetaInfo = {
+            redirectedIdentifier: CONSTANTS.PRODUCT_CARD_MODULE_NAMES.PDP,
+            redirectedSectionName: this.outOfStock ? 'similar_product_oos' : 'similar_products'
+        }
+    }
+
+    getProductPopularDeals() {
+        this.productService.getSimilarProducts(this.productName, this.categoryCode, this.partNumber, this.groupId).subscribe((response: any) => {
+            let products = response['products'];
+            if (products && (products as []).length > 0) {
+                this.polpularDealsProducts = (products as any[]).map(product => this.productService.searchResponseToProductEntity(product));
+                this.popularDealsDataLoaded$.emit(true);
+            }
+        })
+    }
+
+    navigateTo(url) {
+        this.commonService.setSectionClickInformation(this.outOfStock ? 'similar_product_oos' : 'similar_products', 'pdp')
+        this.router.navigateByUrl(url);
+        if (this.commonService.isBrowser) {
+            ClientUtility.scrollToTop(100);
+        }
+    }
+
+}
+
+@NgModule({
+    declarations: [
+        ProductPopularDealsComponent
+    ],
+    imports: [
+        CommonModule,
+        MathFloorPipeModule,
+        MathCeilPipeModule,
+        LazyLoadImageModule,
+        ProductCardVerticalContainerModule,
+        ProductCardVerticalGridViewModule
+    ]
+})
+export class ProductModule { }

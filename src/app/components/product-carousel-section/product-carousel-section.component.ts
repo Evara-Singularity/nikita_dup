5b2d9dca88f18eb1c9dffd32aa85503604bb03c4
Pradeep <sai.pradeep@moglix.com>
2022-06-23 16:48:25 +0530

    ODP-2964 Changes of RFQ pop-up in PLP page

--- src/app/components/product-carousel-section/product-carousel-section.component.ts
+++ src/app/components/product-carousel-section/product-carousel-section.component.ts
@@ -1,78 +1,96 @@
-import { Component, NgModule, Input, EventEmitter, Output } from '@angular/core';
+import { Component, NgModule, Input, EventEmitter, Output, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArrayFilterPipeModule } from "@app/utils/pipes/k-array-filter.pipe";
 import { ProductService } from '@app/utils/services/product.service';
 
 @Component({
-  selector: 'app-product-carousel-section',
-  templateUrl: './product-carousel-section.component.html',
-  styleUrls: ['./product-carousel-section.component.scss']
+	selector: 'app-product-carousel-section',
+	templateUrl: './product-carousel-section.component.html',
+	styleUrls: ['./product-carousel-section.component.scss']
 })
-export class ProductCarouselSectionComponent {
+export class ProductCarouselSectionComponent implements OnInit, AfterViewInit
+{
 
-  @Input('productAllImages') productAllImages;
-  @Input('carouselInitialized') carouselInitialized;
-  @Input('isPurcahseListProduct') isPurcahseListProduct;
-  @Input('productOutOfStock') productOutOfStock;
-  @Input('imagePathAsset') imagePathAsset;
-  @Input('isProductCrouselLoaded') isProductCrouselLoaded;
-  @Input('productCrouselInstance') productCrouselInstance;
-  @Input('iOptions') iOptions;
-  @Input('productTags') productTags;
-  @Input('productName') productName;
-  @Output() loadProductShare: EventEmitter<any> = new EventEmitter<any>();
-  @Output() addToPurchaseList: EventEmitter<any> = new EventEmitter<any>();
-  @Output() scrollToId: EventEmitter<any> = new EventEmitter<any>();
-  @Output() openPopUpcrousel: EventEmitter<any> = new EventEmitter<any>();
-  @Output() onRotateNext: EventEmitter<any> = new EventEmitter<any>();
-  @Output() onRotatePrevious: EventEmitter<any> = new EventEmitter<any>();
-  @Output() loadProductCrousel: EventEmitter<any> = new EventEmitter<any>();
-  @Output() sendProductImageClickTracking: EventEmitter<any> = new EventEmitter<any>();
+	@Input('productAllImages') productAllImages;
+	@Input('carouselInitialized') carouselInitialized;
+	@Input('isPurcahseListProduct') isPurcahseListProduct;
+	@Input('productOutOfStock') productOutOfStock;
+	@Input('imagePathAsset') imagePathAsset;
+	@Input('isProductCrouselLoaded') isProductCrouselLoaded;
+	@Input('productCrouselInstance') productCrouselInstance;
+	@Input('iOptions') iOptions;
+	@Input('productTags') productTags;
+	@Input('productName') productName;
+	@Output() loadProductShare: EventEmitter<any> = new EventEmitter<any>();
+	@Output() addToPurchaseList: EventEmitter<any> = new EventEmitter<any>();
+	@Output() scrollToId: EventEmitter<any> = new EventEmitter<any>();
+	@Output() openPopUpcrousel: EventEmitter<any> = new EventEmitter<any>();
+	@Output() onRotateNext: EventEmitter<any> = new EventEmitter<any>();
+	@Output() onRotatePrevious: EventEmitter<any> = new EventEmitter<any>();
+	@Output() loadProductCrousel: EventEmitter<any> = new EventEmitter<any>();
+	@Output() sendProductImageClickTracking: EventEmitter<any> = new EventEmitter<any>();
+	
 
-  constructor(public productService: ProductService) { }
+	constructor(public productService: ProductService) { }
 
-  loadProductcrousel(i) {
-    this.loadProductCrousel.emit(i);
-  }
+	ngOnInit(): void
+	{
+		
+	}
 
-  loadProductshare() {
-    this.loadProductShare.emit();
-  }
+	ngAfterViewInit(): void
+	{
+	}
 
-  addToPurchaselist() {
-    this.addToPurchaseList.emit();
-  }
+	loadProductcrousel(i)
+	{
+		this.loadProductCrousel.emit(i);
+	}
 
-  scrollToid(data) {
-    this.scrollToId.emit(data);
-  }
+	loadProductshare()
+	{
+		this.loadProductShare.emit();
+	}
 
-  openPopUpCrousel(data) {
-    this.openPopUpcrousel.emit(data);
-  }
+	addToPurchaselist()
+	{
+		this.addToPurchaseList.emit();
+	}
 
-  onRotatenext() {
-    this.onRotateNext.emit();
-  }
+	scrollToid(data)
+	{
+		this.scrollToId.emit(data);
+	}
 
-  onRotateprevious() {
-    this.onRotatePrevious.emit();
-  }
+	openPopUpCrousel(data)
+	{
+		this.openPopUpcrousel.emit(data);
+	}
 
-  sendProductImageClicktracking() {
-    this.sendProductImageClickTracking.emit();
-  }
+	onRotatenext()
+	{
+		this.onRotateNext.emit();
+	}
 
+	onRotateprevious()
+	{
+		this.onRotatePrevious.emit();
+	}
+
+	sendProductImageClicktracking()
+	{
+		this.sendProductImageClickTracking.emit();
+	}
 }
 
 @NgModule({
-  declarations: [ProductCarouselSectionComponent],
-  imports: [
-    CommonModule,
-    ArrayFilterPipeModule
-  ],
-  exports: [
-    ProductCarouselSectionComponent
-  ]
+	declarations: [ProductCarouselSectionComponent],
+	imports: [
+		CommonModule,
+		ArrayFilterPipeModule
+	],
+	exports: [
+		ProductCarouselSectionComponent
+	]
 })
 export class ProductCarouselSectionModule { }

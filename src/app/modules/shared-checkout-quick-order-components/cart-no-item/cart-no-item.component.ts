146b14110f632ca8548dff5d9ed7b6d5822b9192
Pradeep <sai.pradeep@moglix.com>
2022-06-28 20:49:07 +0530

    ODP-2994 integrated with trending categories api

--- src/app/modules/shared-checkout-quick-order-components/cart-no-item/cart-no-item.component.ts
+++ src/app/modules/shared-checkout-quick-order-components/cart-no-item/cart-no-item.component.ts
@@ -1,7 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { DataService } from '@utils/services/data.service';
+import { NullTemplateVisitor } from '@angular/compiler';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CategoryData } from '@app/utils/models/categoryData';
 import { CommonService } from '@app/utils/services/common.service';
 import { CONSTANTS } from '@config/constants';
 import { CartService } from '@services/cart.service';
+import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'environments/environment';
+import { ENDPOINTS } from '@app/config/endpoints';
 
 @Component({
     selector: 'cart-no-item',
@@ -9,14 +16,28 @@ import { CartService } from '@services/cart.service';
     styleUrls: ['./cart-no-item.component.scss'],
 })
 
-export class CartNoItemComponent implements OnInit{
+export class CartNoItemComponent implements OnInit, OnDestroy
+{
 
     readonly imagePath = CONSTANTS.IMAGE_BASE_URL;
+    flyOutData: CategoryData[] = [];
+    flyOutDataSubscription: Subscription = null;
 
-    constructor(public cartService: CartService, private _commonService:CommonService){}
+    constructor(public cartService: CartService, private _dataService: DataService) { }
+
 
     ngOnInit(): void
     {
-        this._commonService.getTrendingCategories().subscribe((data)=>console.log(data))
+        const FLYOUT_URL = environment.BASE_URL + ENDPOINTS.GET_FDK_HOME;
+        this.flyOutDataSubscription = this._dataService.callRestful("GET", FLYOUT_URL).pipe(map((response: CategoryData[]) =>
+        {
+            if (response && response['data']) { return response['data']; }
+            return [];
+        })).subscribe((data) => this.flyOutData = data);
+    }
+
+    ngOnDestroy(): void
+    {
+        this.flyOutDataSubscription.unsubscribe()
     }
 }

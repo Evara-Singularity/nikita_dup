63c51caf41e8e089bf1eb881d7cf16cf0f77b023
Moglix <nikita.yadav@moglix.com>
2022-06-30 12:21:16 +0530

    required changes

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -227,7 +227,7 @@ export class SearchComponent implements OnInit {
     }
 
    
-    digitalData['page']['totalProductCount'] = ((this._productListService?.productListingData?.totalCount) > (this._productListService?.productListingData?.products?.length)) ? (this._productListService.productListingData.totalCount) : (this._productListService.productListingData.products.length);        
+    digitalData['page']['totalProductCount'] = this._productListService?.productListingData?.totalCount;
     digitalData['page']['categoryRecommended'] = relatedSearchResults;
     digitalData['page']['categoryRecSelected'] = this.recommendedCategory;
     digitalData['page']['subSection'] += (this.API_RESULT['searchData'][0].productSearchResult['products'].length === 0 ? " : ZSR" : '');

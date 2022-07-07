30fa3b38c6a56fe7d5826f177b512dc5682042ef
Yogender Singh <yogendersingh@Yogenders-MacBook-Pro.local>
2022-07-07 12:44:53 +0530

    minor changes

--- src/app/components/home-fixed-header/home-fixed-header.module.ts
+++ src/app/components/home-fixed-header/home-fixed-header.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeFixedHeaderComponent } from './home-fixed-header.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [HomeFixedHeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [HomeFixedHeaderComponent]
 })

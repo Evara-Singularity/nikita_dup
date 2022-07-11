097a8a4cbd1632e737cddc3d76eb3f0ec50ca882
LinaShinde <lina.angularminds@monglix.com>
2022-07-11 14:19:33 +0530

    added untracked file

---
+++ src/app/components/product-more-offers/product-more-offers.component.ts
@@ -0,0 +1,47 @@
+import { Component, EventEmitter, Input, NgModule, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PopUpModule } from '../../modules/popUp/pop-up.module';
+import { ModalModule } from '../../modules/modal/modal.module';
+import { BottomMenuModule } from '@app/modules/bottomMenu/bottom-menu.module';
+
+@Component({
+  selector: 'app-product-more-offers',
+  templateUrl: './product-more-offers.component.html',
+  styleUrls: ['./product-more-offers.component.scss']
+})
+export class ProductMoreOffersComponent implements OnInit {
+
+  @Input() data: any ;
+  @Output() out: EventEmitter<any> = new EventEmitter<any>();
+  @Output() isLoading : EventEmitter<any> = new EventEmitter<any>();
+  promoCodeOffers: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const promos = [...this.data];
+    this.promoCodeOffers= promos.splice(1);
+    this.isLoading.emit(false);
+  }
+
+  outData(data) {
+    this.out.emit(data);
+  }
+  
+  togglePopUp(){}
+  
+}
+
+
+@NgModule({
+  declarations: [ProductMoreOffersComponent],
+  imports: [
+    CommonModule,
+    PopUpModule,
+    ModalModule,
+    BottomMenuModule
+  ]
+})
+export default class ProductMoreOffersModule {
+
+}

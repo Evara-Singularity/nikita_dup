595c37dde29801cda9529a24390a29f25efde1d8
Vaibhav <vaibhav.sharma@moglix.com>
2022-07-13 15:32:01 +0530

    ODP-2893-ui-revamp

--- server.ts
+++ server.ts
@@ -100,53 +100,10 @@ function appendImagePreloads(indexHtml) {
       ${bodyPart}
   `;
 
-  newIndexHtml = newIndexHtml.split("ng-transition").join('data-ng-transition'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc58").join('data-_ngcontent-sc58');
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc66").join('data-_ngcontent-sc66');
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc67").join('data-_ngcontent-sc67'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc68").join('data-_ngcontent-sc68'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc69").join('data-_ngcontent-sc69'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc70").join('data-_ngcontent-sc70'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc71").join('data-_ngcontent-sc71'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc72").join('data-_ngcontent-sc72'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc73").join('data-_ngcontent-sc73'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc75").join('data-_ngcontent-sc75'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc76").join('data-_ngcontent-sc76'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc91").join('data-_ngcontent-sc91'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc24").join('data-_ngcontent-sc24'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc26").join('data-_ngcontent-sc26'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc27").join('data-_ngcontent-sc27'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc59").join('data-_ngcontent-sc59'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc62").join('data-_ngcontent-sc62'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc63").join('data-_ngcontent-sc63');
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc64").join('data-_ngcontent-sc64');
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc65").join('data-_ngcontent-sc65'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc28").join('data-_ngcontent-sc28'); 
-  newIndexHtml = newIndexHtml.split("::ng-deep").join(''); 
   newIndexHtml = newIndexHtml.split(";column-count:#343434").join(''); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc103").join('data-_ngcontent-sc103'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc96").join('data-_ngcontent-sc96'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc20").join('data-_ngcontent-sc20'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc80").join('data-_ngcontent-sc80'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc90").join('data-_ngcontent-sc90'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc83").join('data-_ngcontent-sc83'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc88").join('data-_ngcontent-sc88'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc86").join('data-_ngcontent-sc86'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc81").join('data-_ngcontent-sc81');
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc77").join('data-_ngcontent-sc77'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc78").join('data-_ngcontent-sc78'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc79").join('data-_ngcontent-sc79'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc82").join('data-_ngcontent-sc82'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc85").join('data-_ngcontent-sc85'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc87").join('data-_ngcontent-sc87'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc89").join('data-_ngcontent-sc89'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc92").join('data-_ngcontent-sc92'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc96").join('data-_ngcontent-sc96'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc100").join('data-_ngcontent-sc100'); 
-  newIndexHtml = newIndexHtml.split("_ngcontent-sc101").join('data-_ngcontent-sc101'); 
-  newIndexHtml = newIndexHtml.split("ng-reflect-target").join('data-ng-reflect-target'); 
-  newIndexHtml = newIndexHtml.split("ng-reflect-router-link").join('data-reflect-router-link'); 
-  
+  newIndexHtml = newIndexHtml.split("ng-transition").join('data-ng-transition'); 
+  newIndexHtml = newIndexHtml.split("ng-reflect").join('data-ng-reflect'); 
+  newIndexHtml = newIndexHtml.split("_ngcontent").join('data-_ngcontent');
   return newIndexHtml;
 }
 

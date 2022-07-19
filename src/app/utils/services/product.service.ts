f9689b618c43fbd924ab5e321147a103e2a8ade0
LinaShinde <lina.angularminds@monglix.com>
2022-07-19 16:55:51 +0530

    fixed discount of bestseller is wrong bug

--- src/app/utils/services/product.service.ts
+++ src/app/utils/services/product.service.ts
@@ -640,6 +640,7 @@ export class ProductService {
     }
 
     searchResponseToProductEntity(product: any) {
+        console.log("product --->>>", product)
         const partNumber =
             product["partNumber"] ||
             product["defaultPartNumber"] ||
@@ -750,7 +751,8 @@ export class ProductService {
             brandId: brandId,
             brandName: brandName || product['short_description'],
             quantityAvailable: 1,
-            discount: (((productMrp - priceWithoutTax) / productMrp) * 100).toFixed(0),
+           // discount: (((productMrp - priceWithoutTax) / productMrp) * 100).toFixed(0),
+            discount: this._commonService.calculcateDiscount(product['discount_percentage'], productMrp,  product['sellingPrice']),
             rating: null,
             categoryCodes: null,
             taxonomy: null,

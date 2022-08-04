86f1ef8fb5fa47a85371f040e332d25d654c0a43
Yogender Singh <yogendersingh@Yogenders-MacBook-Pro.local>
2022-08-04 21:38:57 +0530

    redirectio issue fixed

--- src/app/modules/bottom-navigation/bottom-navigation.component.ts
+++ src/app/modules/bottom-navigation/bottom-navigation.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Router, RouterModule } from '@angular/router';
 import { LocalAuthService } from '@utils/services/auth.service';
 import { CommonService } from '@app/utils/services/common.service';
+import { LocalStorageService } from 'ngx-webstorage';
 
 
 
@@ -19,18 +20,31 @@ export class BottomNavigationComponent implements OnInit {
   constructor(
     private router: Router,
     private localAuthService: LocalAuthService,
-    public _commonService: CommonService
-
+    public _commonService: CommonService,
+    private localStorageService: LocalStorageService,
     ) { }
 
   ngOnInit() {
     this.isUserLogin = this.localAuthService.isUserLoggedIn();
   }
 
-  bottomNavRedirection(url){
+  bottomNavRedirection(url, title = "") {
     this.menuSelected=true;
-    this.router.navigate([url]);
+    // this.router.navigate([url]);
+    this.checkIfUserLoggedIn(url, title);
+  }
 
+  checkIfUserLoggedIn(url, title = "") {
+    let user = this.localStorageService.retrieve("user");
+    if (user && user.authenticated == "true") {
+      this.router.navigate([url]);
+    } else {
+      this.localAuthService.setBackURLTitle(url, title);
+      let navigationExtras: NavigationExtras = {
+        queryParams: { 'backurl': url },
+      };
+      this.router.navigate(['/login'], navigationExtras);
+    }
   }
 
   redirectionToSearchPopUp(){

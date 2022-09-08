adfd95fc92a0bc1a7f18c64db78d6c4944586b56
Pradeep <sai.pradeep@moglix.com>
2022-09-08 13:48:24 +0530

    Fixed cod popup issue in payment page

--- src/app/utils/services/cart.service.ts
+++ src/app/utils/services/cart.service.ts
@@ -1522,6 +1522,7 @@ export class CartService
             {
                 this._toastService.show({ type: 'error', text: 'Product successfully removed from Cart' });
                 const ITEM_LIST = tempCartSession['itemsList'];
+                
                 if (ITEM_LIST && ITEM_LIST.length == 0 && this._router.url.indexOf('/checkout') != -1) {
                     this.clearBuyNowFlow();
                     // clears browser history so they can't navigate with back button
@@ -1532,7 +1533,6 @@ export class CartService
                     tempCartSession['cart']['totalOffer'] = totalOffer;
                     tempCartSession['extraOffer'] = null;
                     this._notifyCartChanges(tempCartSession, null);
-                    this.updateNonDeliverableItemsAfterRemove(tempCartSession['itemsList']);
                 }
                 // 50 ms wait time for cartItems to update after product removed
                 setTimeout(() => {
@@ -1545,7 +1545,6 @@ export class CartService
     updateNonDeliverableItemsAfterRemove(cartItems:any[])
     {
         const freshmsns = cartItems.map((item)=>item.productId);
-        console.log(freshmsns);
         let tempcods:any[] = [];
         if (this.codNotAvailableObj['itemsArray'])
         {

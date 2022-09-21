de82696527520e02f79b408378889d3146c65257
Moglix <nikita.yadav@moglix.com>
2022-09-21 16:49:12 +0530

    basic code for gold Membership

---
+++ src/app/modules/goldMembership/goldMembership.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goldMembership',
+  templateUrl: './goldMembership.component.html',
+  styleUrls: ['./goldMembership.component.scss']
+})
+export class GoldMembershipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}

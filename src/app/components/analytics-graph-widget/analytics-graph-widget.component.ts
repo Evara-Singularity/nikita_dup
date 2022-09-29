8de3bd00ad52bda670604ac7a3428ce9133043a5
pooja-maury <pooja.maurya@moglix.com>
2022-09-29 11:59:13 +0530

    add multiple analytics graph html

--- src/app/components/analytics-graph-widget/analytics-graph-widget.component.ts
+++ src/app/components/analytics-graph-widget/analytics-graph-widget.component.ts
@@ -12,214 +12,284 @@ import { filter } from 'rxjs-compat/operator/filter';
   styleUrls: ['./analytics-graph-widget.component.scss']
 })
 export class AnalyticsGraphWidgetComponent implements OnInit {
-  graphData :Array<any>=[];
+  graphData: Array<any> = [];
   chartOptions = {};
   @Input() chartType;
   @Input() filterData: Array<any>;
-  @Input() categoryId:any;
-  fragmentPriceObject:any;
-  
-  brandName:'';
-  bucketData:any;
-  attributeLength:any;
+  @Input() categoryId: any;
+  fragmentPriceObject: any;
+
+  brandName: '';
+  bucketData: any;
+  attributeLength: any;
 
   priceDataWithoutProcessing;
   brandDataWithoutProcessing;
   attributeDataWithoutProcessing;
-  
 
-  constructor(private dataService:DataService,private commonService:CommonService,private _productListService:ProductListService) { }
+
+  constructor(private dataService: DataService, private commonService: CommonService, private _productListService: ProductListService) { }
 
   ngOnInit(): void {
-    console.log("categoryId",this.categoryId);
+    console.log("categoryId", this.categoryId);
     this.getData();
   }
-      
-  callChartApi(){
+
+  callChartApi() {
     let url = 'http://localhost:3000/graphData';
     // let url = environment.BASE_URL + ENDPOINTS.GET_CATEGORY_ANALYTICS + "?categoryCode=" +this.categoryId;
     return this.dataService.callRestful("GET", url);
-  } 
+  }
 
-  getData(){
+  getData() {
     this.callChartApi().subscribe(res => {
-      if(res[0].statusCode == 200){
-        this.graphData = res[0]['data']; 
-      // if(res['statusCode'] == 200){
-      //   this.graphData = res['data']; 
+      if (res[0].statusCode == 200) {
+        this.graphData = res[0]['data'];
+        // if(res['statusCode'] == 200){
+        //   this.graphData = res['data']; 
         this.graphData.forEach(element => {
-          if(element.block_name == 'attribute_report'){
-            if(element.data && element.data.length >0){
-            this.attributeDataWithoutProcessing = element.data;
+          if (element.block_name == 'attribute_report') {
+            if (element.data && element.data.length > 0) {
+              this.attributeDataWithoutProcessing = element.data;
               element.data.forEach((item, index) => {
                 setTimeout(() => {
-                  this.loadChart(`attribute-chart${index}`,item['attributePercentange'], this.prepareAttributeChartData(item['attributePercentange']))
-                },0);
-             });
+                  this.loadChart(`attribute-chart${index}`, item['attributePercentange'], this.prepareAttributeChartData(item['attributePercentange']))
+                }, 0);
+              });
             }
           }
-          else if(element.block_name == 'product_report'){
+          else if (element.block_name == 'product_report') {
             let priceData = [];
-            if(element.data && element.data.length >0){
-            this.priceDataWithoutProcessing = element.data;
+            if (element.data && element.data.length > 0) {
+              this.priceDataWithoutProcessing = element.data;
               element.data.forEach(price => {
                 priceData = [...priceData, ...this.preparePriceChartData(price)]
-                console.log("priceData",priceData);
+                console.log("priceData", priceData);
               });
-            
-            setTimeout(() => {
-              this.loadChart('price-chart',priceData,priceData);
-             },10);
+
+              setTimeout(() => {
+                this.loadChart('price-chart', priceData, priceData);
+              }, 10);
             }
           }
-          else{
-            if(element.data && element.data.length >0){
+          else {
+            if (element.data && element.data.length > 0) {
               let brandData = [];
               this.brandDataWithoutProcessing = element.data;
               element.data.forEach(brand => {
-               brandData = [...brandData, ...this.prepareBrandChartData(brand)]
+                brandData = [...brandData, ...this.prepareBrandChartData(brand)]
               });
               setTimeout(() => {
-                this.loadChart('brand-chart',brandData,brandData);
-               }, 20);
-              }
+                this.loadChart('brand-chart', brandData, brandData);
+              }, 20);
+            }
           }
         });
-       }
-       else{
-         console.log("error");
-       }
+      }
+      else {
+        console.log("error");
+      }
     });
   }
   //create Chart Data
-  prepareAttributeChartData(attributeData){
+  prepareAttributeChartData(attributeData) {
     const seriesAttributeArray = [];
-      for(var attr in attributeData){
-        let itemObj = {};
-        itemObj['name'] = attr.toString();
-        itemObj['y'] = attributeData[attr];
-        itemObj['drilldown'] = null;
-        seriesAttributeArray.push(itemObj);
-      }
+    for (var attr in attributeData) {
+      let itemObj = {};
+      itemObj['name'] = attr.toString();
+      itemObj['y'] = attributeData[attr];
+      itemObj['drilldown'] = null;
+      seriesAttributeArray.push(itemObj);
+    }
     return seriesAttributeArray;
   }
-  prepareBrandChartData(brandData){
-  //  debugger;
+  prepareBrandChartData(brandData) {
+    //  debugger;
     const seriesBrandArray = [];
-      let brandObj = {};
-      brandObj['name'] = brandData['brandName'].toString();
-      brandObj['y'] = brandData['orderPercentage'];
-      brandObj['drilldown'] = null;
-      seriesBrandArray.push(brandObj);
-    
-    return seriesBrandArray; 
-  }  
-  
-  preparePriceChartData(priceData){
-    console.log("priceData",priceData);
+    let brandObj = {};
+    brandObj['name'] = brandData['brandName'].toString();
+    brandObj['y'] = brandData['orderPercentage'];
+    brandObj['drilldown'] = null;
+    seriesBrandArray.push(brandObj);
+
+    return seriesBrandArray;
+  }
+
+  preparePriceChartData(priceData) {
+    console.log("priceData", priceData);
     const seriesPriceArray = [];
     // for(var price in priceData){
-      let priceObj = {};
-      priceObj['name'] = priceData['interval'].toString();
-      priceObj['y'] = priceData['orderPercentage'];
-      priceObj['drilldown'] = null;
-      seriesPriceArray.push(priceObj);
-    
-    return seriesPriceArray; 
+    let priceObj = {};
+    priceObj['name'] = priceData['interval'].toString();
+    priceObj['y'] = priceData['orderPercentage'];
+    priceObj['drilldown'] = null;
+    seriesPriceArray.push(priceObj);
+
+    return seriesPriceArray;
   }
-  createChartOptionsObject(data,seriesArray){
+  createChartOptionsObject(data, seriesArray) {
     let chartOptions = {
-          chart: {
-              type: 'column'
-          },
-          title: {
-              align: 'left',
-              text: 'Browser market shares. January, 2022'
-          },
-          subtitle: {
-              align: 'left',
-              text: 'Click the columns to view versions. Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
-          },
-          accessibility: {
-              announceNewData: {
-                  enabled: true
-              }
-          },
-          xAxis: {
-              type: 'category'
-          },
-          yAxis: {
-              title: {
-                  text: data
-              }
-          },
-          legend: {
-              enabled: false
-          },
-          plotOptions: {
-              series: {
-                  borderWidth: 0,
-                  dataLabels: {
-                      enabled: true,
-                      format: '{point.y:.1f}%'
-                  }
-              }
-          },
-          tooltip: {
-              headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-              pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
-          },
-          series: [
-              {
-                  name: "Browsers",
-                  colorByPoint: true,
-                  data:seriesArray
-              }
-          ]
-      } 
-      return chartOptions; 
+      chart: {
+        type: 'column',
+        spacingLeft: 10,
+        spacingRight: 10,
+        spacingTop: 10,
+        width: 270,
+        height: 280,
+      },
+      title: {
+        align: 'left',
+        text: ''
+      },
+      subtitle: {
+        enabled: false
+      },
+      credits: {
+        enabled: false
+      },
+      exporting: {
+        enabled: false,
+      },
+      accessibility: {
+        announceNewData: {
+          enabled: true
+        }
+      },
+      xAxis: {
+        type: 'category'
+      },
+      yAxis: {
+        title: {
+          text: data
+        }
+      },
+      legend: {
+        enabled: false
+      },
+      plotOptions: {
+        series: {
+          borderWidth: 0,
+          dataLabels: {
+            enabled: true,
+            format: '{point.y:.1f}%'
+          }
+        }
+      },
+      tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+      },
+      series: [
+        {
+          name: "Browsers",
+          colorByPoint: true,
+          data: seriesArray
+        }
+      ]
+    }
+    return chartOptions;
+  }
+  createChartSingleObject(data, seriesArray) {
+    let chartOptions = {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        align: 'left',
+        text: 'Yajya  '
+      },
+      subtitle: {
+        align: 'left',
+        text: 'Click the columns to view versions. Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
+      },
+      accessibility: {
+        announceNewData: {
+          enabled: true
+        }
+      },
+      xAxis: {
+        type: 'category'
+      },
+      yAxis: {
+        title: {
+          text: data
+        }
+      },
+      legend: {
+        enabled: false
+      },
+      plotOptions: {
+        series: {
+          borderWidth: 0,
+          dataLabels: {
+            enabled: true,
+            format: '{point.y:.1f}%'
+          }
+        }
+      },
+      tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+      },
+      series: [
+        {
+          name: "Browsers",
+          colorByPoint: true,
+          data: seriesArray
+        }
+      ]
+    }
+    return chartOptions;
   }
 
-  createCHartAttribute(attributeData, graphData){
-    this.createChartOptionsObject(attributeData,graphData);
-  }
 
-  loadChart(htmlId, data, seriesData){
-    console.log('graph info', htmlId , data, seriesData);
-    var Highcharts = require('highcharts');  
+  // createCHartAttribute(attributeData, graphData){
+  //   this.createChartOptionsObject(attributeData,graphData);
+  // }
+
+  loadChart(htmlId, data, seriesData) {
+    console.log('graph info', htmlId, data, seriesData);
+    var Highcharts = require('highcharts');
     // Load module after Highcharts is loaded
-    require('highcharts/modules/exporting')(Highcharts); 
+    require('highcharts/modules/exporting')(Highcharts);
     Highcharts.chart(
       htmlId,
-      this.createChartOptionsObject(data,seriesData)
+      this.createChartSingleObject(data, seriesData)
     );
+    if (htmlId.startsWith('attribute-chart')) {
+      console.log("true")
+      Highcharts.chart(
+        htmlId,
+        this.createChartOptionsObject(data, seriesData)
+      );
+    }
   }
- 
-  generateFragmentUrl(filterName,filterValue){
+
+  generateFragmentUrl(filterName, filterValue) {
     let fragmentPriceObject = {};
-     if(filterName == 'price'){
+    if (filterName == 'price') {
       fragmentPriceObject['price'] = [filterValue.toString()];
-      this.commonService.selectedFilterData.filter = fragmentPriceObject; 
+      this.commonService.selectedFilterData.filter = fragmentPriceObject;
       this.commonService.applyFilter();
-     }
-     else if(filterName == 'brand'){
+    }
+    else if (filterName == 'brand') {
       let fragmentBrandObject = {
         'brand': [filterValue.toString()]
       }
       fragmentPriceObject['brand'] = [filterValue.toString()];
-      this.commonService.selectedFilterData.filter = fragmentPriceObject; 
+      this.commonService.selectedFilterData.filter = fragmentPriceObject;
       this.commonService.applyFilter();
-     }
-    else{
+    }
+    else {
       fragmentPriceObject[filterName] = [filterValue.toString()];
-      this.commonService.selectedFilterData.filter = fragmentPriceObject; 
+      this.commonService.selectedFilterData.filter = fragmentPriceObject;
       this.commonService.applyFilter();
     }
   }
 
-  formatPrice(value:string){
+  formatPrice(value: string) {
     let formatValue = value.split(',');
-    return (formatValue[0]+'-'+formatValue[1]);
+    return (formatValue[0] + '-' + formatValue[1]);
   }
 }
 

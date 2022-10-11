e87cbf99aacc40f3067460ef489e27d1eeab4ce8
Yogender Singh <yogendersingh@Yogenders-MacBook-Pro.local>
2022-10-11 14:47:30 +0530

    Fixed ODP3678 & ODP3679

--- src/app/modules/shared-auth-popup/shared-auth-popup.component.ts
+++ src/app/modules/shared-auth-popup/shared-auth-popup.component.ts
@@ -1,17 +1,19 @@
 import { Router, RouterModule } from '@angular/router';
-import { Component, EventEmitter, Input, NgModule, OnDestroy, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, NgModule, OnDestroy, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedAuthModule } from '../shared-auth-v1/shared-auth.module';
 import { BottomMenuModule } from '../bottomMenu/bottom-menu.module';
 import { SharedAuthUtilService } from '../shared-auth-v1/shared-auth-util.service';
 import { LocalAuthService } from '@app/utils/services/auth.service';
+import { LocalStorageService } from 'ngx-webstorage';
+import { CommonService } from '@app/utils/services/common.service';
 
 @Component({
   selector: "auth",
   templateUrl: "./shared-auth-popup.component.html",
   styleUrls: ["./shared-auth-popup.component.scss"],
 })
-export class SharedAuthPopUpComponent implements OnInit,OnDestroy {
+export class SharedAuthPopUpComponent implements OnInit,OnDestroy, AfterViewInit {
   @Input() flow: string;
   @Input() redirectUrl: string = null;
   @Output() removeAuthComponent$: EventEmitter<any> = new EventEmitter<any>();
@@ -19,16 +21,19 @@ export class SharedAuthPopUpComponent implements OnInit,OnDestroy {
   constructor(
     private _sharedAuthUtilService: SharedAuthUtilService, 
     private _localAuthService: LocalAuthService, 
+    public localStorageService: LocalStorageService,
+    private _commonService: CommonService,
     private _router: Router ) { }
 
   ngOnInit(): void { 
-    console.log('change flow ==>', this.redirectUrl);
     this._sharedAuthUtilService.sendLoginPopUpTracking();
   }
 
+  ngAfterViewInit(): void {
+    this._commonService.setBodyScroll(null, false);
+  }
 
   togglePopUp(value) {
-    
     this.flow = value;
   }
 
@@ -42,9 +47,11 @@ export class SharedAuthPopUpComponent implements OnInit,OnDestroy {
   }
 
   removeAuthComponent() {
-    if(this.redirectUrl){
+    let userSession = this.localStorageService.retrieve('user');
+    if(this.redirectUrl && userSession && userSession.authenticated == "true" ){
       this._router.navigateByUrl(this.redirectUrl);
     }
+    this._commonService.setBodyScroll(null, true);
     this.removeAuthComponent$.emit();
   }
 

8cab2dff040b064a189be8ed33fa5e01fcd7ded5
Moglix <nikita.yadav@moglix.com>
2022-11-30 17:33:46 +0530

    timing change from 8 to 6 PM

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -419,7 +419,7 @@ export const CONSTANTS = {
   NEFT_AMOUNT_LMIT: 2000,
   EMI_MINIMUM_AMOUNT: 3000,
   CUSTOMER_CARE_TIME:{
-    'call_timing_text': 'Call Timings: 9:00 AM - 8:00 PM (Monday to Saturday)'
+    'call_timing_text': 'Call Timings: 9:00 AM - 6:00 PM (Monday to Saturday)'
   },
   DEFAULT_USER_NAME_PLACE_HOLDER:'User',
   SEARCH_ABT_FLAG: 'y',

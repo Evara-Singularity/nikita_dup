adb68beaf28d749ba665fe5e4060716060bba719
Yogender Singh <yogendersingh@Yogenders-MacBook-Pro.local>
2022-12-01 18:05:13 +0530

    contact us changes

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -28,8 +28,8 @@ export class ContactComponent {
 
     this.isServer = isPlatformServer(this.platformId)
 
-    this.title.setTitle("Contact Us - Moglix - Call 8448 233 444");
-    this.meta.addTag({ "property": "og:title", "content": "Contact Us - Moglix - Call 8448 233 444" });
+    this.title.setTitle("Contact Us - Moglix - WhatsApp at +91 9999049135");
+    this.meta.addTag({ "property": "og:title", "content": "Contact Us - Moglix - WhatsApp at +91 9999049135" });
     this.meta.addTag({ "property": "og:description", "content": "Write to us at care[at]moglix.com or contact customer care at 8448 233 444. We are happy to help you round the clock to the best of our ability." });
     this.meta.addTag({ "property": "og:url", "content": CONSTANTS.PROD + "/contact" });
     this.meta.addTag({ "name": "description", "content": "Write to us at care[at]moglix.com or contact customer care at 8448 233 444. We are happy to help you round the clock to the best of our ability." });

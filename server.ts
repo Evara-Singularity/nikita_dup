87305faa2cf85146d9266e77924de95f6ce95db5
Vaibhav <vaibhav.sharma@moglix.com>
2022-12-20 13:17:21 +0530

    seo-changes-languageTrans

--- server.ts
+++ server.ts
@@ -67,7 +67,7 @@ export function app() {
       requestLogObj.processTime = requestLogObj.endTime - requestLogObj.startTime;
       console.log('PageLoadTimeLog :', requestLogObj); 
       if(html){
-        res.status(html ? res.statusCode : 500).send(appendImagePreloads(html) || err.message);
+        res.status(html ? res.statusCode : 500).send(appendImagePreloads(html,requestLogObj['originalUrl']) || err.message);
       }else{
         res.status(500).send(err.message || `<h1>Something went wrong.</h1>${req.url}`);
       }
@@ -86,7 +86,7 @@ function shouldCompress (req, res) {
   return compression.filter(req, res)
 }
 
-function appendImagePreloads(indexHtml) {
+function appendImagePreloads(indexHtml, url) {
   const regexImage = /<img.*?src=".*?"/g
   const regexImageSrc = /src=".*?"/g
   // maxLimit is to make sure only images coming in first view ports are being preloaded.
@@ -123,6 +123,12 @@ function appendImagePreloads(indexHtml) {
   newIndexHtml = newIndexHtml.split("ng-transition").join('data-ng-transition'); 
   newIndexHtml = newIndexHtml.split("ng-reflect").join('data-ng-reflect'); 
   newIndexHtml = newIndexHtml.split("_ngcontent").join('data-_ngcontent');
+  if(url.includes('/hi')){
+    newIndexHtml = newIndexHtml.split('<html lang="en">').join('<html lang="hi">');
+  }
+  else {
+    newIndexHtml = newIndexHtml.split('<html lang="hi">').join('<html lang="en">');
+  }
   return newIndexHtml;
 }
 

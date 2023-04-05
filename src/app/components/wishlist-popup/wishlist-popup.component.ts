a43ce121f3100dc66a4d1a616316a91fb11e57ab
jaisurya-moglix <jaisurya.kataria@moglix.com>
2023-04-05 12:46:00 +0530

    wishlist popup and coupons code done.

---
+++ src/app/components/wishlist-popup/wishlist-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wishlist-popup',
+  templateUrl: './wishlist-popup.component.html',
+  styleUrls: ['./wishlist-popup.component.scss']
+})
+export class WishlistPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/wishlist-popup/wishlist-popup.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WishlistPopupComponent } from './wishlist-popup.component';
+
+
+
+@NgModule({
+  declarations: [WishlistPopupComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class WishlistPopupModule { }

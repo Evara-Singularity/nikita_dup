c8743fe3315e3c7ce7f5eac8eef3c66d103af5cd
jaisurya-moglix <jaisurya.kataria@moglix.com>
2023-04-13 17:37:29 +0530

    addTo multiple products in wishlist done.

--- src/app/modules/shared-checkout-unavailable-items/shared-checkout-unavailable-items.component.ts
+++ src/app/modules/shared-checkout-unavailable-items/shared-checkout-unavailable-items.component.ts
@@ -1,45 +1,105 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { CartService } from '@app/utils/services/cart.service';
-import { Subject } from 'rxjs';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { CONSTANTS } from "@app/config/constants";
+import { ENDPOINTS } from "@app/config/endpoints";
+import { LocalAuthService } from "@app/utils/services/auth.service";
+import { CartService } from "@app/utils/services/cart.service";
+import { DataService } from "@app/utils/services/data.service";
+import { forkJoin, Subject } from "rxjs";
+import { map } from "rxjs/operators";
+import { ToastMessageService } from "../toastMessage/toast-message.service";
 
 @Component({
-    selector: 'shared-checkout-unavailable-items',
-    templateUrl: './shared-checkout-unavailable-items.component.html',
-    styleUrls: ['./shared-checkout-unavailable-items.component.scss']
+  selector: "shared-checkout-unavailable-items",
+  templateUrl: "./shared-checkout-unavailable-items.component.html",
+  styleUrls: ["./shared-checkout-unavailable-items.component.scss"],
 })
-export class SharedCheckoutUnavailableItemsComponent implements OnInit
-{
-    @Output() closePopup$: EventEmitter<any> = new EventEmitter<any>();
-    @Input() data: any;
-    @Input() user: {};
-    @Input() showLink = true;
-    private cDistroyed = new Subject();
-    itemsList: [] = [];
+export class SharedCheckoutUnavailableItemsComponent implements OnInit {
+  @Output() closePopup$: EventEmitter<any> = new EventEmitter<any>();
+  @Input() data: any;
+  @Input() user: {};
+  @Input() showLink = true;
+  private cDistroyed = new Subject();
+  itemsList: [] = [];
+  wishListPostBody = [];
 
-    constructor(public _cartService: CartService) { }
+  constructor(
+    public _cartService: CartService,
+    public _localAuthService: LocalAuthService,
+    private _dataService: DataService,
+    private _tms: ToastMessageService
+  ) {}
 
-    ngOnInit() { 
-        this.itemsList = this.data['items']; 
-    }
+  ngOnInit() {
+    this.itemsList = this.data["items"];
+    const userSession = this._localAuthService.getUserSession();
+    this.itemsList.forEach((elememt) => {
+      this.wishListPostBody.push({
+        idUser: userSession.userId,
+        userType: "business",
+        idProduct: elememt["productId"] || elememt["defaultPartNumber"],
+        productName: elememt["productName"],
+        description: elememt["productDescripton"],
+        brand: elememt["brandName"],
+        category: elememt["categoryCode"],
+      });
+    });
+  }
 
-    onUpdate($event) {
-        this._cartService.showUnavailableItems = false;
-    }
+  onUpdate($event) {
+    this._cartService.showUnavailableItems = false;
+  }
 
+  removeUnavailableItems(callback) {
+    this.closeModal();
+    callback(this.itemsList);
+  }
 
-    removeUnavailableItems(callback)
-    {
+  closeModal() {
+    this._cartService.showUnavailableItems = false;
+  }
+
+  addToWishList() {
+    const allApis = [];
+    this.wishListPostBody.forEach((ele) => {
+      const url = CONSTANTS.NEW_MOGLIX_API + ENDPOINTS.ADD_PURCHASE_LIST;
+      const addToWishListApi = this._dataService
+        .callRestful("POST", url, { body: ele })
+        .pipe(
+          map((res) => {
+            return res;
+          })
+        );
+      allApis.push(addToWishListApi);
+    });
+
+    forkJoin(allApis).subscribe(
+      (response) => {
+        if (response["status"]) {
+          this._tms.show({
+            type: "success",
+            text: "Product added to wishlist.",
+          });
+          this.closeModal();
+        } else {
+          this._tms.show({
+            type: "success",
+            text: response["errorMessage"],
+          });
+          this.closeModal();
+        }
+      },
+      (error) => {
+        this._tms.show({
+          type: "success",
+          text: "Something went wrong.",
+        });
         this.closeModal();
-        callback(this.itemsList);
-    }
+      }
+    );
+  }
 
-    closeModal(){
-        this._cartService.showUnavailableItems = false;
-    }
-
-    ngOnDestroy()
-    {
-        this.cDistroyed.next();
-        this.cDistroyed.unsubscribe();
-    }
+  ngOnDestroy() {
+    this.cDistroyed.next();
+    this.cDistroyed.unsubscribe();
+  }
 }

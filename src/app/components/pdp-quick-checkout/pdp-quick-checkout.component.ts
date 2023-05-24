c86cb4837f51bac1026fb3f1efb93a870b4678f7
anilkumargopisetti <anil.payasia@moglix.com>
2023-05-24 15:52:57 +0530

    added setTimeOut for copon issue

--- src/app/components/pdp-quick-checkout/pdp-quick-checkout.component.ts
+++ src/app/components/pdp-quick-checkout/pdp-quick-checkout.component.ts
@@ -169,10 +169,12 @@ export class PdpQuickCheckoutComponent implements OnInit {
 
   ngAfterViewInit() {
     this.currUser = this.localAuthService.getUserSession();
-    this.cartService.getPromoCodesByUserId(this.currUser["userId"]);
     this.shippmentCharge = this.cartService.shippingCharges;
-    this.cartService.shippingAddress = this.shippingAddress
-    this.cartService.billingAddress = this.billingAddress
+    this.cartService.shippingAddress = this.shippingAddress;
+    this.cartService.billingAddress = this.billingAddress;
+    setTimeout(() => {
+      this.cartService.getPromoCodesByUserId(this.currUser["userId"]);
+    }, 200);
   }
 
   addTocart(productDetails, buyNow) {

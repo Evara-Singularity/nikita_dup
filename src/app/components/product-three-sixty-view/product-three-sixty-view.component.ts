1c85c742a64d4fbe0851a382eff207e60e12fbec
yajya <yajya.kalra@moglix.com>
2023-06-22 14:46:31 +0530

    poc

---
+++ src/app/components/product-three-sixty-view/product-three-sixty-view.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+// Rest of your TypeScript code goes here
+@Component({
+  selector: 'product-three-sixty-view',
+  templateUrl: './product-three-sixty-view.component.html',
+  styleUrls: ['./product-three-sixty-view.component.scss']
+})
+export class ProductThreeSixtyViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/product-three-sixty-view/product-three-sixty-view.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductThreeSixtyViewComponent } from './product-three-sixty-view.component';
+
+@NgModule({
+  declarations: [
+    ProductThreeSixtyViewComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    ProductThreeSixtyViewComponent
+  ]
+})
+export class ProductThreeSixtyViewModule { }

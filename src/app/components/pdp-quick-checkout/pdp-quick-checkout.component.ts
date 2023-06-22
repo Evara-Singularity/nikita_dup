efc10981bf49eefd5a991821d1eb6aa5e94d5f8f
Yogender Singh <Yogi@MOGNOILTM1446.local>
2023-06-22 15:45:30 +0530

    address popup removed

--- src/app/components/pdp-quick-checkout/pdp-quick-checkout.component.ts
+++ src/app/components/pdp-quick-checkout/pdp-quick-checkout.component.ts
@@ -156,15 +156,15 @@ export class PdpQuickCheckoutComponent implements OnInit {
     }); 
     this.setAddress(this.address, true);
     this.cartService.appliedPromoCode = "";
-    this.promoSubscription = this.cartService.promoCodeSubject.subscribe(
-      ({ promocode, isNewPromocode }) => {
-        this.showPromoSuccessPopup = isNewPromocode;
-        setTimeout(() => {
-          this.getUpdatedCart();
-          this.showPromoSuccessPopup = false;
-        }, 800);
-      }
-    );
+    // this.promoSubscription = this.cartService.promoCodeSubject.subscribe(
+    //   ({ promocode, isNewPromocode }) => {
+    //     this.showPromoSuccessPopup = isNewPromocode;
+    //     setTimeout(() => {
+    //       this.getUpdatedCart();
+    //       this.showPromoSuccessPopup = false;
+    //     }, 800);
+    //   }
+    // );
   }
 
   ngAfterViewInit() {
@@ -470,7 +470,7 @@ export class PdpQuickCheckoutComponent implements OnInit {
 
   ngOnDestroy(): void {
     if (this.cartSubscription) this.cartSubscription.unsubscribe();
-    if (this.promoSubscription) this.promoSubscription.unsubscribe();
+    // if (this.promoSubscription) this.promoSubscription.unsubscribe();
   }
 }
 

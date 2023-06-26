3b115d0c6550606443356c5b47b59547b588bd98
Yogender Singh <Yogi@MOGNOILTM1446.local>
2023-06-26 21:55:31 +0530

    featured prodcuts added

--- src/app/modules/adsense/adsense-feature-products-unit.module.ts
+++ src/app/modules/adsense/adsense-feature-products-unit.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeatureProductsUnitComponent } from './feature-products-unit/feature-products-unit.component';
-
-
+import { ProductCardVerticalContainerModule } from '../ui/product-card-vertical-container/product-card-vertical-container.module';
+import { ProductCardVerticalGridViewModule } from '../product-card/product-card-vertical-grid-view/product-card-vertical-grid-view.module';
 
 @NgModule({
   declarations: [FeatureProductsUnitComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ProductCardVerticalContainerModule,
+    ProductCardVerticalGridViewModule,
   ],
   exports: [FeatureProductsUnitComponent]
 })
--- src/app/modules/adsense/feature-products-unit/feature-products-unit.component.ts
+++ src/app/modules/adsense/feature-products-unit/feature-products-unit.component.ts
@@ -1,14 +1,54 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
+import { ProductCardFeature, ProductCardMetaInfo, ProductsEntity } from '@app/utils/models/product.listing.search';
+import { ProductService } from '@app/utils/services/product.service';
+import { ProductListService } from '@app/utils/services/productList.service';
 
 @Component({
   selector: 'adsense-feature-products-unit',
   templateUrl: './feature-products-unit.component.html',
   styleUrls: ['./feature-products-unit.component.css']
 })
-export class FeatureProductsUnitComponent {
+export class FeatureProductsUnitComponent implements AfterViewInit {
 
   @Input() data: string[] | null = null;
+  @Input('pageName') pageName = "pdp";
+  @Input('moduleUsedIn') moduleUsedIn = "PRODUCT_RECENT_PRODUCT";
+  @Input() analytics = null;
+  public msnListData: ProductsEntity[] = null;
+  readonly cardFeaturesConfig: ProductCardFeature = {
+    // feature config
+    enableAddToCart: true,
+    enableBuyNow: true,
+    enableFeatures: false,
+    enableRating: true,
+    enableVideo: false,
+    // design config
+    enableCard: true,
+    verticalOrientation: true,
+    horizontalOrientation: false,
+    lazyLoadImage: false,
+  }
+  cardMetaInfo: ProductCardMetaInfo = null;
 
-  constructor() { }
+  constructor(
+    private _productService: ProductService,
+    private _productListService: ProductListService,
+  ) { }
+
+
+  ngAfterViewInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+
+    if (this.data && this.data.length > 0) {
+      this._productService.getProductList(this.data).subscribe(result => {
+        if (result && result['searchProductList'] && result['searchProductList'].length > 0) {
+          this.msnListData = this._productListService.getSerachProductList(result['searchProductList']);
+        }
+      })
+    }
+  }
 
 }

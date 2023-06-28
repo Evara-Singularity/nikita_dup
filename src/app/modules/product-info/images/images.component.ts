4ecd6a173f6b6a1cda0af5539e5e655b9cc722c6
yajya <yajya.kalra@moglix.com>
2023-06-28 17:13:26 +0530

    poc-changes

--- src/app/modules/product-info/images/images.component.ts
+++ src/app/modules/product-info/images/images.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ComponentFactoryResolver, Injector, ViewContainerRef, ViewChild, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ObserveVisibilityDirectiveModule } from '../../../utils/directives/observe-visibility.directive';
+import { PopUpVariant2Module } from '../../pop-up-variant2/pop-up-variant2.module';
 
 @Component({
     selector: 'images',
@@ -7,10 +10,18 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ImagesComponent implements OnInit
 {
+    product3dInstance = null;
+    @ViewChild("product3dContainerRef", { read: ViewContainerRef })
+    product3dContainerRef: ViewContainerRef;
+    show360popupFlag:boolean = false;
     @Input("images") images: any[] = null;
     currentImageIndex = -1;
 
-    constructor() { }
+    constructor(
+        private _componentFactoryResolver:ComponentFactoryResolver,
+        private injector:Injector
+    ) {
+     }
 
     ngOnInit(): void
     {
@@ -19,4 +30,20 @@ export class ImagesComponent implements OnInit
 
     updateImageIndex(index) { this.currentImageIndex = index; }
 
+    async load360ViewComponent(){
+        this.show360popupFlag = true;
+            const { ProductThreeSixtyViewComponent } = await import('../../../components/product-three-sixty-view/product-three-sixty-view.component');
+            const factory = this._componentFactoryResolver.resolveComponentFactory(ProductThreeSixtyViewComponent);
+            this.product3dInstance = this.product3dContainerRef.createComponent(
+            factory, 
+            null, 
+            this.injector
+        );
+    } 
+    outData(e){
+
+    }
 }
+
+    
+

56573d00f44920f05c0ca9ba9fcb0efb6e7cc79f
jaisurya-moglix <jaisurya.kataria@moglix.com>
2023-07-04 13:15:19 +0530

    Prod-ab baseurl chnages with -vpn  done.

--- src/environments/environment.prodab.ts
+++ src/environments/environment.prodab.ts
@@ -3,7 +3,7 @@ export const environment = {
   enableServerLogs: true,
   PROD_NAME: 'https://www.moglix.com',
   // BASE_URL: 'https://apinew.moglix.com/nodeApi/v1',
-  BASE_URL: 'https://apinew.moglix.com/nodeApi/v1',
+  BASE_URL: 'https://apinew-vpn.moglix.com/nodeApi/v1',
   BASE_URL_V2: 'https://apinew.moglix.com/nodeApi/v2',
   //BASE_URL: 'https://nodeapiqa.moglilabs.com/nodeApi/v1',
   APP_BASE_URL: 'https://api.moglix.com',

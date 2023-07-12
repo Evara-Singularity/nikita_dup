52405e1932819f67b1b466e54085b00d57516ce8
yajya <yajya.kalra@moglix.com>
2023-07-12 18:07:29 +0530

    color configuration will be added from frontEnd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,13 @@ export const environment = {
   enableServerLitogs: false,
   PROD_NAME: 'https://www.moglix.com',
   
-  BASE_URL: 'https://apinew.moglix.com/nodeApi/v1',
-  BASE_URL_V2: 'https://apinew.moglix.com/nodeApi/v2', 
-  APP_BASE_URL: 'https://api.moglix.com',
+  // BASE_URL: 'https://apinew.moglix.com/nodeApi/v1',
+  // BASE_URL_V2: 'https://apinew.moglix.com/nodeApi/v2', 
+  // APP_BASE_URL: 'https://api.moglix.com',
 
-  // BASE_URL: 'https://nodeapiqa.moglilabs.com/nodeApi/v1',
-  // BASE_URL_V2: 'https://nodeapiqa.moglilabs.com/nodeApi/v2',
-  // APP_BASE_URL: 'https://apiqa.moglilabs.com',
+  BASE_URL: 'https://nodeapiqa.moglilabs.com/nodeApi/v1',
+  BASE_URL_V2: 'https://nodeapiqa.moglilabs.com/nodeApi/v2',
+  APP_BASE_URL: 'https://apiqa.moglilabs.com',
 
   IMAGE_ASSET_URL: '/',
   SOCKET_URL: "https://socketqa.moglilabs.com",  

461044229f5f14aa557e1f588e2ade9fb6a6c4ff
Moglix <nikita.yadav@moglix.com>
2023-07-14 13:02:33 +0530

    analytics monet

--- src/app/modules/adsense/adsense-main-banner.module.ts
+++ src/app/modules/adsense/adsense-main-banner.module.ts
@@ -1,19 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MainAdsenseBannerComponent } from './main-banner/main-banner.component';
-import { NgxSiemaModule } from 'ngx-siema';
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MainAdsenseBannerComponent } from "./main-banner/main-banner.component";
+import { NgxSiemaModule } from "ngx-siema";
+import { ObserveVisibilityDirectiveModule } from "@app/utils/directives/observe-visibility.directive";
 
 @NgModule({
-  declarations: [
-    MainAdsenseBannerComponent
-  ],
+  declarations: [MainAdsenseBannerComponent],
   imports: [
     CommonModule,
     NgxSiemaModule.forRoot(),
+    ObserveVisibilityDirectiveModule,
   ],
-  exports: [
-    MainAdsenseBannerComponent
-  ]
+  exports: [MainAdsenseBannerComponent],
 })
-export class AdsenseMainBannerModule { }
+export class AdsenseMainBannerModule {}
--- src/app/modules/adsense/adsense-promoted-brands-unit.module.ts
+++ src/app/modules/adsense/adsense-promoted-brands-unit.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PromotedBrandsUnitComponent } from './promoted-brands-unit/promoted-brands-unit.component';
+import { ObserveVisibilityDirectiveModule } from '@app/utils/directives/observe-visibility.directive';
 
 
 
 @NgModule({
-  declarations: [
-    PromotedBrandsUnitComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    PromotedBrandsUnitComponent
-  ]
+  declarations: [PromotedBrandsUnitComponent],
+  imports: [CommonModule, ObserveVisibilityDirectiveModule],
+  exports: [PromotedBrandsUnitComponent],
 })
-export class AdsensePromotedBrandsUnitModule { }
+export class AdsensePromotedBrandsUnitModule {}

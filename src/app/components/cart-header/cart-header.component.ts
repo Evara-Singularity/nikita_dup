5e925df256bc96256e37e457f2e813db26907d55
Moglix <nikita.yadav@moglix.com>
2023-09-18 19:01:00 +0530

    sprint 32 staging without nudge popUp

--- src/app/components/cart-header/cart-header.component.ts
+++ src/app/components/cart-header/cart-header.component.ts
@@ -26,16 +26,6 @@ export class CartHeaderComponent implements OnInit, OnDestroy
 	cartUpdatesSubscription: Subscription = null;
 	orderId = null;
 
-	backButtonClickQuickOrderSubscription: Subscription;
-    isBackClickedQuickOrder: boolean=false; 
-	backButtonClickPaymentSubscription: Subscription;
-    isBackClickedPayment: boolean=false; 
-	cancelIconClickedSubscription: Subscription;
-    isCancelIconClicked: boolean=true;
-	cancelIconClickedPaymentSubscription: Subscription;
-    isCancelIconPaymentClicked: boolean=true;
-	missOutSavingAmount: number=0; //calculate [mrp-(selling Price + shipping Charges)]
-
 	constructor(
 		public _commonService: CommonService,
 		public _cartService: CartService,
@@ -46,11 +36,6 @@ export class CartHeaderComponent implements OnInit, OnDestroy
 
 	ngOnInit(): void
 	{
-		this._naviagtionService.setBackClickedQuickOrder(false);
-		this._naviagtionService.setBackClickedPayment(false);
-		this._naviagtionService.setCancelIconQuickOrderClicked(true);
-		this._naviagtionService.setCancelIconPaymentClicked(true);
-
 		this.cartUpdatesSubscription = this._cartService.getCartUpdatesChanges().subscribe(cartSession =>
 		{
 			//front end created dummy cart session;
@@ -61,68 +46,16 @@ export class CartHeaderComponent implements OnInit, OnDestroy
 			}
 		});
 		this.noOfCartItems = this._cartService.getCartItemsCount();
-		this.backButtonClickQuickOrderSubscription = this._naviagtionService.isBackClickedQuickOrder$.subscribe(
-			value => {
-			  this.isBackClickedQuickOrder = value;
-			}
-		  );
-		
-		this.backButtonClickPaymentSubscription = this._naviagtionService.isBackClickedPayment$.subscribe(
-		value => {
-			this.missOutSavingAmount=this.calculate_mrp_totalPayable_Difference();
-			this.isBackClickedPayment = value;
-		}
-		);
-		this.cancelIconClickedSubscription = this._naviagtionService.isCancelIconQuickOrderClicked$.subscribe(
-			value => {
-			  this.isCancelIconClicked = value;
-			}
-		  );  
-		  this.cancelIconClickedPaymentSubscription = this._naviagtionService.isCancelIconPaymentClicked$.subscribe(
-			value => {
-			  this.isCancelIconPaymentClicked = value;
-			}
-		  );    
 	}
-	calculate_mrp_totalPayable_Difference() { 
-		const sums = this._cartService.getGenericCartSession["itemsList"].reduce((acc, item) => {
-		  acc.sum_mrpAmounts += (item.amount * item.productQuantity);
-		  acc.sum_totalPayableAmounts += (item.totalPayableAmount + item.shippingCharges);
-		  return acc;
-		}, { sum_mrpAmounts: 0, sum_totalPayableAmounts: 0 });
-		return sums.sum_mrpAmounts - sums.sum_totalPayableAmounts;
-	  }	
+
 
 	handleNavigation()
 	{
 		if (this.isCheckout && this._cartService.buyNow) {
 			this._cartService.clearBuyNowFlow();
 		}
-		if (this.isQuickorder && !this.isBackClickedQuickOrder && this.isCancelIconClicked ) {
-			this._naviagtionService.setBackClickedQuickOrder(true);
-			// this.resetCartChanges();
-			// this.goBack$.emit();
-			return
-		}
-		if (this.isQuickorder && this.isBackClickedQuickOrder && !this.isCancelIconClicked ) {
-			this._naviagtionService.setBackClickedQuickOrder(false);
+		if (this.isQuickorder) {
 			this.resetCartChanges();
-			this.goBack$.emit();
-			return
-		}
-		if(this.isPayment && !this.isBackClickedPayment && this.isCancelIconPaymentClicked && this.missOutSavingAmount){
-			this._naviagtionService.setBackClickedPayment(true);
-			return
-		}
-		if(this.isPayment && this.isBackClickedPayment && this.isCancelIconPaymentClicked && this.missOutSavingAmount){
-			this._naviagtionService.setBackClickedPayment(true);
-			this.goBack$.emit();
-			return
-		}
-		if(this.isPayment && !this.missOutSavingAmount){
-			this._naviagtionService.setBackClickedPayment(true);
-			this.goBack$.emit();
-			return
 		}
 		this.goBack$.emit();
 	}
@@ -156,10 +89,6 @@ export class CartHeaderComponent implements OnInit, OnDestroy
 	ngOnDestroy(): void
 	{
 		if (this.cartUpdatesSubscription) { this.cartUpdatesSubscription.unsubscribe(); }
-		if (this.backButtonClickQuickOrderSubscription) this.backButtonClickQuickOrderSubscription.unsubscribe();
-		if (this.backButtonClickPaymentSubscription) this.backButtonClickPaymentSubscription.unsubscribe();
-		if (this.cancelIconClickedSubscription) this.cancelIconClickedSubscription.unsubscribe();
-		if (this.cancelIconClickedPaymentSubscription) this.cancelIconClickedPaymentSubscription.unsubscribe();
 	}
 
 }

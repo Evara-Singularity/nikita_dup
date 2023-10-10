f1b4ec2d3215510a74daa1db790aa18d6de14a09
anilkumargopisetti <anil.payasia@moglix.com>
2023-10-10 13:48:42 +0530

    added logs for adobe check

--- src/app/utils/services/global-analytics.service.ts
+++ src/app/utils/services/global-analytics.service.ts
@@ -34,7 +34,7 @@ export class GlobalAnalyticsService {
 
   sendAdobeCall(data: any, trackingname = "genericPageLoad") {
     // console.log(environment["ISCHROME"]);
-    // console.log("sendAdobeCall   :: data =====>", data);
+    console.log("sendAdobeCall   :: data =====>", data);
     if (this.isBrowser && _satellite != undefined && _satellite.track) {
       digitalData = Object.assign({}, data);
       // console.log('digitalData', digitalData);
@@ -97,6 +97,7 @@ export class GlobalAnalyticsService {
 
   //this method gives basic common tracking object
   getCommonTrackingObject(product?, channel?, linkName?) {
+    console.log(product, channel, linkName);
     let tracking = { page: {}, order: {}, custData: this.custDataTracking };
     tracking['page']['channel'] = channel ? channel : "";
     tracking['page']['linkName'] = linkName ? linkName : "";
